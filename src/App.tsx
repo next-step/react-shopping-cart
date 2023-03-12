bb7915393158c692d0236b895a6e04822fb09596
yujeong <yujeong.choung@gmail.com>
2023-03-13 02:05:21 +0900

    feat: 쇼핑카트 프로젝트 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App"></div>
+    </>
+  );
+}
+
+export default App;
