de72bfe5dda1eb2c5b94b4baec8f62b53a9816e5
ag502 <ag502@naver.com>
2023-03-12 16:22:07 +0900

    add: 예외처리 헬퍼 함수 구현

---
+++ src/helpers/index.ts
@@ -0,0 +1,42 @@
+import type { GeneralFunction } from "@/types";
+
+type AsyncTryFunction<T> = () => Promise<T>;
+
+type WithAsyncReturn<T> = {
+  result: T | null;
+  error: unknown;
+};
+
+export async function withAsync<T>(fn: AsyncTryFunction<T>): Promise<WithAsyncReturn<T>> {
+  try {
+    const result = await fn();
+
+    return {
+      result,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      result: null,
+      error,
+    };
+  }
+}
+
+export function tryCatch(tryFunction: GeneralFunction, catchFunction?: GeneralFunction) {
+  try {
+    const result = tryFunction();
+
+    return {
+      result,
+      error: null,
+    };
+  } catch (error) {
+    catchFunction?.(error);
+
+    return {
+      result: null,
+      error,
+    };
+  }
+}
