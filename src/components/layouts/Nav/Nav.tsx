407f6f1db2bbfde49f2a454192e727faf65a7123
alwayshere <76519867+herekim@users.noreply.github.com>
2023-03-13 20:26:46 +0900

    feat: Nav 컴포넌트 Link 태그 추가

--- src/components/layouts/Nav/Nav.tsx
+++ src/components/layouts/Nav/Nav.tsx
@@ -1,12 +1,22 @@
+import { Link } from 'react-router-dom'
+
 const Nav = () => {
   return (
     <nav className="nav flex justify-around">
       <div className="flex-center">
-        <h1 className="nav-title">CLEAN CODE SHOP</h1>
+        <h1 className="nav-title">
+          <Link to="/">
+            <button>CLEAN CODE SHOP</button>
+          </Link>
+        </h1>
       </div>
       <div className="flex gap-15">
-        <button className="nav-button">장바구니</button>
-        <button className="nav-button">주문목록</button>
+        <button className="nav-button">
+          <Link to="/cart">장바구니</Link>
+        </button>
+        <button className="nav-button">
+          <Link to="/order-list">주문목록</Link>
+        </button>
       </div>
     </nav>
   )
