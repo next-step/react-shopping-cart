f89a523d93cc365da5da343f7ca8d60b5c52bc3e
이영우 <ywlee@hanatour.com>
2023-03-13 12:25:41 +0900

    [feat] mock api module, cart type, cart api 호출 추가

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import { Cart as CartType } from "../types/cart";
+import { mockApi } from "../utils/mockApi";
+
 function Cart() {
+  const [carts, setCarts] = useState<CartType[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // TODO: real api 로 수정
+        const carts = await mockApi.get<CartType[]>("/carts");
+        setCarts(carts);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
   return <div>Cart</div>;
 }
 
