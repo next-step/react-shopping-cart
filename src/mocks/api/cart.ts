1645e4c4bfb71209856dc038a2d2b3c09783e760
신형기 <tkaqkeldk@hanatour.com>
2023-03-13 14:25:24 +0900

    refactor: mocks api > cart, product return data naming 변경

--- src/mocks/api/cart.ts
+++ src/mocks/api/cart.ts
@@ -2,6 +2,6 @@
 
 export const getCarts = async () => {
   const response = await fetch("/api/carts");
-  const products = await response.json();
-  return products;
+  const data = await response.json();
+  return data;
 };
--- src/mocks/api/product.ts
+++ src/mocks/api/product.ts
@@ -1,5 +1,5 @@
 export const getProducts = async () => {
   const response = await fetch("/api/products");
-  const products = await response.json();
-  return products;
+  const data = await response.json();
+  return data;
 };
