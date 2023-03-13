79229acf6d98f59dd1fae5a8d36cbb9e12ff7e74
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-13 21:23:43 +0900

    mod: MSW Deffered mount 처리

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function prepare() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./mocks/browser');
+    return worker.start();
+  }
+  return Promise.resolve();
+}
+prepare().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
