b6c1c2989c5c26f656bc653982a140c824fa5bef
김민혁 <alsgur7408@gmail.com>
2023-03-14 22:58:19 +0900

    feat: 페이지 라우터 파일 추가

--- server.js
+++ server.js
@@ -12,65 +12,65 @@ import { JSONFile } from 'lowdb/node';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
-const adapter = new JSONFile(path.join(__dirname, "db.json"));
+const adapter = new JSONFile(path.join(__dirname, 'db.json'));
 const db = new Low(adapter);
 
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
 
-server.post("/products", (req, res) => {
+server.post('/products', (req, res) => {
   const { price, name, imageUrl } = req.body;
-
+  
   if (
     !Number.isInteger(price) ||
-    typeof name !== "string" ||
-    typeof imageUrl !== "string"
+    typeof name !== 'string' ||
+    typeof imageUrl !== 'string'
   ) {
     res.sendStatus(400);
   } else {
-    db.get("products").push({ id: Date.now(), price, name, imageUrl }).write();
+    db.get('products').push({ id: Date.now(), price, name, imageUrl }).write();
     res.sendStatus(201);
   }
 });
 
-server.post("/carts", (req, res) => {
+server.post('/carts', (req, res) => {
   const { product } = req.body;
   const { price, name, imageUrl } = product;
-
+  
   if (
     !Number.isInteger(price) ||
-    typeof name !== "string" ||
-    typeof imageUrl !== "string"
+    typeof name !== 'string' ||
+    typeof imageUrl !== 'string'
   ) {
     res.sendStatus(400);
   } else {
-    db.get("carts").push({ id: Date.now(), product }).write();
+    db.get('carts').push({ id: Date.now(), product }).write();
     res.sendStatus(201);
   }
 });
 
-server.post("/orders", (req, res) => {
+server.post('/orders', (req, res) => {
   const { orderDetails } = req.body;
-
+  
   for (const orderDetail of orderDetails) {
     const { quantity, price, name, imageUrl } = orderDetail;
-
+    
     if (
       !Number.isInteger(quantity) ||
       quantity < 1 ||
       !Number.isInteger(price) ||
-      typeof name !== "string" ||
-      typeof imageUrl !== "string"
+      typeof name !== 'string' ||
+      typeof imageUrl !== 'string'
     ) {
       res.sendStatus(400);
       return;
     }
   }
-
-  db.get("orders")
+  
+  db.get('orders')
     .push({
       id: Date.now(),
       orderDetails,
@@ -83,5 +83,5 @@ server.post("/orders", (req, res) => {
 server.use(router);
 
 server.listen(3003, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,31 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { List } from '../pages';
+import { Cart, Detail, List, Order, OrderDetail, OrderList } from '../pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <List/>,
   },
+  {
+    path: '/cart',
+    element: <Cart/>,
+  },
+  {
+    path: '/detail',
+    element: <Detail/>,
+  },
+  {
+    path: '/order',
+    element: <Order/>,
+  },
+  {
+    path: '/order-detail',
+    element: <OrderDetail/>,
+  },
+  {
+    path: '/order-list',
+    element: <OrderList/>,
+  }
 ]);
 
 export default router;
