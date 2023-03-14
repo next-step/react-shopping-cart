b63ada604f63aa6d3bd76c7e2b857d07f697cb49
김민혁 <alsgur7408@gmail.com>
2023-03-14 23:17:13 +0900

    feat: storybook 추가

---
+++ src/components/molecules/header/stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Header from './index';
+
+export default {
+  title: 'Common/Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header/>;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {
+    name: 'Jane Doe',
+  },
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
