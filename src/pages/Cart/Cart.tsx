7f1ce0b3e9a8181f24781f43d137f9a9945488a2
alwayshere <76519867+herekim@users.noreply.github.com>
2023-03-14 22:36:58 +0900

    feat: CartItem 컴포넌트 추가

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 import { SubHeader } from '@/components'
 
+import { CartItem } from './components'
 import { useCart } from './hooks'
 
 const Cart = () => {
@@ -23,26 +24,8 @@ const Cart = () => {
             <h3 className="cart-title">든든배송 상품(3개)</h3>
             <hr className="divide-line-gray mt-10" />
             {cartList?.map((item) => (
-              <div key={item.id} className="cart-container">
-                <div className="flex gap-15 mt-10">
-                  <input className="checkbox" name="checkbox" type="checkbox" checked={true} />
-                  <img className="w-144 h-144" src="./assets/images/product.png" alt={item.name} />
-                  <span className="cart-name">{item.name}</span>
-                </div>
-                <div className="flex-col-center justify-end gap-15">
-                  <img className="cart-trash-svg" src="./assets/svgs/trash.svg" alt="삭제" />
-                  <div className="number-input-container">
-                    <input type="number" className="number-input" value="1" />
-                    <div>
-                      <button className="number-input-button">▲</button>
-                      <button className="number-input-button">▼</button>
-                    </div>
-                  </div>
-                  <span className="cart-price">{`${item.price.toLocaleString()}원`}</span>
-                </div>
-              </div>
+              <CartItem key={item.id} item={item} />
             ))}
-
             <hr className="divide-line-thin mt-10" />
           </section>
         </div>
