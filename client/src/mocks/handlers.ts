e4e9a5b01d8249c805702717703ceafc6579be9e
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-15 00:10:03 +0900

    mod: 주문내역 Mock API 더미데이터 업데이트

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,10 +1,10 @@
 import { rest } from 'msw';
 
 import { IProduct } from 'types/product';
-import dummy from './dummy.json';
+import { products } from './data';
 
 export const handlers = [
   rest.get<IProduct[]>('/products', (_, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(1000), ctx.json(dummy.products));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(products));
   }),
 ];
