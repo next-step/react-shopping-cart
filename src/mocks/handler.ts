38a43e6241108ff768fea8c4125dc9dde32797fa
이영우 <ywlee@hanatour.com>
2023-03-15 22:57:21 +0900

    [feat] mock api 에서 post 도 가능하게끔 수정

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,21 +1,34 @@
+import { Cart } from "./../types/cart";
 import { rest } from "msw";
 import MOCK from "./data.json";
+
+const state = {
+  carts: MOCK.carts,
+  orders: MOCK.orders,
+  products: MOCK.products,
+};
 
 export const handlers = [
   rest.get(`/carts`, async (req, res, ctx) => {
     await sleep(200);
 
-    return res(ctx.status(200), ctx.json(MOCK.carts));
+    return res(ctx.status(200), ctx.json(state.carts));
+  }),
+  rest.post(`/carts`, async (req, res, ctx) => {
+    const newPost = req.body as Cart;
+    state.carts.push(newPost);
+    await sleep(200);
+    return res(ctx.status(200), ctx.json(state.carts));
   }),
   rest.get(`/orders`, async (req, res, ctx) => {
     await sleep(200);
 
-    return res(ctx.status(200), ctx.json(MOCK.orders));
+    return res(ctx.status(200), ctx.json(state.orders));
   }),
   rest.get(`/products`, async (req, res, ctx) => {
     await sleep(200);
 
-    return res(ctx.status(200), ctx.json(MOCK.products));
+    return res(ctx.status(200), ctx.json(state.products));
   }),
 ];
 
