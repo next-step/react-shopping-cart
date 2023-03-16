56cde8b906671b030d01407038442e1d94f987e7
ag502 <ag502@naver.com>
2023-03-17 00:13:36 +0900

    fix: SubHeader props에 className 및 textAlign 추가

--- src/components/common/SubHeader/index.tsx
+++ src/components/common/SubHeader/index.tsx
@@ -4,8 +4,14 @@ import * as S from "./subHeader.style";
 
 export interface SubHeaderProps {
   children: string;
+  textAlign?: "left" | "center" | "right";
+  className?: string;
 }
 
-export default function SubHeader({ children }: SubHeaderProps) {
-  return <S.SubHeaderContainer>{children}</S.SubHeaderContainer>;
+export default function SubHeader({ children, textAlign = "center", className }: SubHeaderProps) {
+  return (
+    <S.SubHeaderContainer className={className} textAlign={textAlign}>
+      {children}
+    </S.SubHeaderContainer>
+  );
 }
