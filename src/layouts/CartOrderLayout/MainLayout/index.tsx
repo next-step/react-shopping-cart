bef7886f6c402b0e48e817ef174e50386e2c8179
ag502 <ag502@naver.com>
2023-03-17 01:22:21 +0900

    fix: CartOrderLayout의 도메인 별 SubHeader 제목 분기처리

--- src/layouts/CartOrderLayout/MainLayout/index.tsx
+++ src/layouts/CartOrderLayout/MainLayout/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { SubHeader } from "@/components/common";
+import { DOMAINS } from "@/routes";
 
 import * as S from "./cartOrderMainLayout.style";
 
+const subHeaderTitle = {
+  [DOMAINS.CART]: "장바구니",
+  [DOMAINS.ORDER]: "주문목록",
+};
+
 export default function CartOrderLayout() {
+  const { pathname } = useLocation();
+
   return (
     <div>
-      <SubHeader>장바구니</SubHeader>
+      <SubHeader>{subHeaderTitle[pathname]}</SubHeader>
       <S.CartLayoutContentWrapper>
         <Outlet />
       </S.CartLayoutContentWrapper>
