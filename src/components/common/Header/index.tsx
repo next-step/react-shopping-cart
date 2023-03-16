3ecb3fd83ad10908d0e0422802eb03f829caa0e4
ag502 <ag502@naver.com>
2023-03-17 01:17:35 +0900

    add: Header 컴포넌트에 주문목록 페이지 이동 링크 연결

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,33 +1,26 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-import { DOMAINS } from "@/routes";
+import { useNavigateTo } from "@/hooks";
 
 import * as S from "./header.style";
 
 export default function Header() {
-  const navigate = useNavigate();
-
-  const moveToMainPage = () => {
-    navigate(DOMAINS.MAIN);
-  };
-
-  const moveToCartPage = () => {
-    navigate(DOMAINS.CART);
-  };
+  const moveTo = useNavigateTo();
 
   return (
     <S.HeaderContainer>
       <S.HeaderContentWrapper>
-        <S.HeaderTitleWrapper onClick={moveToMainPage}>
+        <S.HeaderTitleWrapper onClick={moveTo("MAIN")}>
           <S.HeaderCartIcon />
           <S.HeaderTitle>Next Step</S.HeaderTitle>
         </S.HeaderTitleWrapper>
         <S.NavButtonWrapper>
-          <S.NavButton variant="text" onClick={moveToCartPage}>
+          <S.NavButton variant="text" onClick={moveTo("CART")}>
             장바구니
           </S.NavButton>
-          <S.NavButton variant="text">주문목록</S.NavButton>
+          <S.NavButton variant="text" onClick={moveTo("ORDER")}>
+            주문목록
+          </S.NavButton>
         </S.NavButtonWrapper>
       </S.HeaderContentWrapper>
     </S.HeaderContainer>
