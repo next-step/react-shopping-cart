072ff85200a3b53d86cf5a499b7803d1344c7c74
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2023-03-17 00:13:39 +0900

    style: format 기존 파일

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'none',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    singleQuote: true,
-    printWidth: 120,
-    useTabs: false,
-    tabWidth: 2,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2
 };
