cb03b318e95b4af577ed67cd54021284b5c43a16
realhee <wlsgml5628@gmail.com>
2023-03-16 23:55:13 +0900

    Test: MSW API Mocking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import { worker } from "./mocks/worker.js";
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/mocks/handlers/todo.js
@@ -0,0 +1,17 @@
+import { rest } from "msw";
+
+const todos = ["먹기", "자기", "놀기"];
+
+// 요청 핸들러 작성
+export const handlers = [
+  // 할일 목록
+  rest.get("/todos", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(todos));
+  }),
+
+  // 할일 추가
+  rest.post("/todos", (req, res, ctx) => {
+    todos.push(req.body);
+    return res(ctx.status(201));
+  }),
+];
