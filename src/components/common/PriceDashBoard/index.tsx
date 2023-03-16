ed954c8c50312b0fcbe78835de7d91f3ae64ebde
ag502 <ag502@naver.com>
2023-03-16 22:59:22 +0900

    add: PriceDashBoard 컴포넌트 추가

---
+++ src/components/common/PriceDashBoard/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { numberWithCommas } from "@/helpers";
+
+import * as S from "./priceDashBoard.style";
+
+export interface PriceDashBoardProps {
+  title: string;
+  priceTitle: string;
+  price: number;
+  buttonLabel: string;
+  className?: string;
+  onSubmit: () => void;
+}
+
+export default function PriceDashBoard({
+  title,
+  priceTitle,
+  price,
+  buttonLabel,
+  onSubmit,
+  className,
+}: PriceDashBoardProps) {
+  return (
+    <S.PriceDashBoardContainer className={className}>
+      <S.PriceDashBoardTitleWrapper>
+        <S.PriceDashBoardTitle>{title}</S.PriceDashBoardTitle>
+      </S.PriceDashBoardTitleWrapper>
+      <S.PriceDashBoardContentWrapper>
+        <S.PriceWrapper>
+          <span>{priceTitle}</span>
+          <span>{numberWithCommas(price)} 원</span>
+        </S.PriceWrapper>
+        <S.PriceDashBoardButton variant="contained" onClick={onSubmit}>
+          {buttonLabel}
+        </S.PriceDashBoardButton>
+      </S.PriceDashBoardContentWrapper>
+    </S.PriceDashBoardContainer>
+  );
+}
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,4 +2,5 @@ export { default as Button } from "./Button";
 export { default as CheckBox } from "./CheckBox";
 export { default as Header } from "./Header";
 export { default as NumberCounter } from "./NumberCounter";
+export { default as PriceDashBoard } from "./PriceDashBoard";
 export { default as SubHeader } from "./SubHeader";
