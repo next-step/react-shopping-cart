6e2f92d5f4ca95edc248a7824df8fb6838c49304
ag502 <ag502@naver.com>
2023-03-18 12:30:16 +0900

    fix: api port 번호 변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults } from "axios";
 
 const axiosParams: CreateAxiosDefaults = {
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "/",
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
 };
 
 const axiosInstance = axios.create(axiosParams);
