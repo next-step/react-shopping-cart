8e55fa188e4d4689ce9f60b540851817d590daa3
ag502 <ag502@naver.com>
2023-03-18 12:34:20 +0900

    add: gray, white gray 글로벌 색상 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,7 +4,8 @@ export const UTIL_COLORS = {
   CYAN: "#2AC1BC",
   WHITE: "#FFFFFF",
   BLACK: "#000000",
-  WHITE_GRAY: "#BBBBBB",
+  WHITE_GRAY: "#F6F6F6",
+  GRAY: "#AAAAAA",
 };
 
 const GlobalStyle = createGlobalStyle`
@@ -12,7 +13,8 @@ const GlobalStyle = createGlobalStyle`
         --cyan: ${UTIL_COLORS.CYAN};
         --white: ${UTIL_COLORS.WHITE};
         --black: ${UTIL_COLORS.BLACK};
-        --white-gray: ${UTIL_COLORS.WHITE_GRAY}
+        --white-gray: ${UTIL_COLORS.WHITE_GRAY};
+        --gray: ${UTIL_COLORS.GRAY};
     }
 
     body {
