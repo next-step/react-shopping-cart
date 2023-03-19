616cf45e3cedd81d5cfad6d4cb9f7208eb574428
devsoonote <devsoo0527@gmail.com>
2023-03-20 04:34:58 +0900

    fixed: add optional on Products component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -23,16 +23,17 @@ function Products() {
       })
   }, [productHttpClient])
 
-  if (!products.length) return <>nothing</>
   return (
     <div className='container mx-auto my-6'>
-      <ul className='grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-        {products.map((product) => (
-          <li key={product.id}>
-            <Product product={product} />
-          </li>
-        ))}
-      </ul>
+      {products?.length && (
+        <ul className='grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          {products?.map((product) => (
+            <li key={product.id}>
+              <Product product={product} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
