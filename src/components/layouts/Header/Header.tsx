611442a4145ae3c110efb9cb2f4c2dced0dcddf9
alwayshere <76519867+herekim@users.noreply.github.com>
2023-03-19 17:26:22 +0900

    modify: Header 컴포넌트 Link 태그 데이터와 표현 분리

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useProductInfo } from '@/hooks'
@@ -5,29 +6,29 @@ import { useProductInfo } from '@/hooks'
 const Header = () => {
   const { getProductId } = useProductInfo()
 
+  // Todo: 해당 로직 외부로 분리하기
+  const getDestinations = useMemo(() => {
+    // Todo: path 상수로 관리하기 (현재는 변경에 취약)
+    return [
+      { to: '/', text: '상품 목록' },
+      { to: `/detail/${getProductId()}`, text: '상품 상세' },
+      { to: '/cart', text: '장바구니' },
+      { to: '/order', text: '주문/결제' },
+      { to: '/order-list', text: '주문 목록' },
+      { to: '/order-detail', text: '주문 상세' },
+    ]
+  }, [getProductId])
+
   return (
     <header className="flex-col-center">
       <h1 className="global-nav-title">장바구니</h1>
       <br />
       <div className="global-nav-button-box">
-        <Link className="global-nav-button" to="/">
-          상품 목록
-        </Link>
-        <Link className="global-nav-button" to={`/detail/${getProductId()}`}>
-          상품 상세
-        </Link>
-        <Link className="global-nav-button" to="/cart">
-          장바구니
-        </Link>
-        <Link className="global-nav-button" to="/order">
-          주문/결제
-        </Link>
-        <Link className="global-nav-button" to="/order-list">
-          주문 목록
-        </Link>
-        <Link className="global-nav-button" to="/order-detail">
-          주문 상세
-        </Link>
+        {getDestinations.map((destination) => (
+          <Link key={destination.to} className="global-nav-button" to={destination.to}>
+            {destination.text}
+          </Link>
+        ))}
       </div>
     </header>
   )
