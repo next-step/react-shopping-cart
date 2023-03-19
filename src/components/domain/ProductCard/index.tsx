b93ff7663cd7c78ce92dbd025bb81274f0657e91
dahye1013 <alrean@naver.com>
2023-03-20 02:18:51 +0900

    chore: productCard placholder 관련 early return

--- src/components/domain/ProductCard/index.tsx
+++ src/components/domain/ProductCard/index.tsx
@@ -7,35 +7,33 @@ type Props = {
 };
 
 const ProductCard = ({ product }: Props) => {
-  if (product) {
+  if (!product) {
     return (
-      <div>
-        <LazyImage
-          src={product.image}
-          alt={product.name}
-          width={280}
-          height={280}
-        />
-        <div className="flex justify-between w-280 p-5">
-          <div className="product-info">
-            <span className="product-info__name">{product.name}</span>
-            <span className="product-info__price">
-              {currency(product.price)}
-            </span>
-          </div>
-          <button>
-            <CartIcon />
-          </button>
+      <div className="product-card flex-col gap-10">
+        <LazyImage width={280} height={250} />
+        <div className="flex justify-between w-280 h-5">
+          <div className="product-info animated-bg"></div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="product-card flex-col gap-10">
-      <LazyImage width={280} height={250} />
-      <div className="flex justify-between w-280 h-5">
-        <div className="product-info animated-bg"></div>
+    <div>
+      <LazyImage
+        src={product.image}
+        alt={product.name}
+        width={280}
+        height={280}
+      />
+      <div className="flex justify-between w-280 p-5">
+        <div className="product-info">
+          <span className="product-info__name">{product.name}</span>
+          <span className="product-info__price">{currency(product.price)}</span>
+        </div>
+        <button>
+          <CartIcon />
+        </button>
       </div>
     </div>
   );
