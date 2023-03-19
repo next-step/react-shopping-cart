64b5b7d721319bfd68a58a718ca6bdb3b071ef66
devsoonote <devsoo0527@gmail.com>
2023-03-20 02:02:41 +0900

    feat: add CartApiContext

--- src/context/productApiContext.tsx
+++ src/context/productApiContext.tsx
@@ -1,20 +1,23 @@
 import { createContext, PropsWithChildren, useContext } from 'react'
 import React from 'react'
 import { ProductClientType, productHttpClient } from '../api/productHttpClient'
+import { CartHttpClientType, cartHttpClient } from '../api/cartHttpClient'
 
 type ProductApiContextProps = {
   productHttpClient: ProductClientType | null
+  cartHttpClient: CartHttpClientType | null
 }
 
 export const ProductApiContext = createContext<ProductApiContextProps>({
   productHttpClient: null,
+  cartHttpClient: null,
 })
 
 export const ProductApiProvder: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <ProductApiContext.Provider value={{ productHttpClient }}>
+    <ProductApiContext.Provider value={{ productHttpClient, cartHttpClient }}>
       {children}
     </ProductApiContext.Provider>
   )
