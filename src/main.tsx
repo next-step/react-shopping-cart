caa588868158ed04bfc7e2c82977fb2bb0a04e1d
Leebonggu <leebonggu12@hanmail.net>
2023-03-20 23:26:57 +0900

    refactor: msw실행조건문 변경

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,7 @@ import { router } from './RootRouter';
 import { worker } from './mocks/browser';
 
 if (getEnvMode() === 'production' || getEnvMode() === 'development') {
-  try {
-    await worker.start();
-  } catch (error) {
-    console.error(error);
-  }
+  worker.start();
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
