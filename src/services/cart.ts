01bb6facaf62b74d8f20de13e4ea82ddc72fec3f
신형기 <tkaqkeldk@hanatour.com>
2023-03-20 22:59:11 +0900

    refactor: cart.ts 타이핑

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,8 +1,9 @@
 import { get } from "services";
+import { CartItem } from "types/type";
 
 const CARTS = "/carts";
 
-export const getCarts = async () => {
+export const getCarts = async (): Promise<CartItem[]> => {
   const { data } = await get(CARTS);
   return data;
 }
