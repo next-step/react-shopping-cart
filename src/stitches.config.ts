fdaed21ba8efd1aae8677ce4161e35ea3b8fcf88
Woo KyungJun <natcho9010@gmail.com>
2023-03-21 13:22:01 +0900

    chore: add textHighlight util function & mint color

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,11 +1,34 @@
 import { createStitches } from '@stitches/react';
 
 export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
-  theme: {},
+  theme: {
+    colors: {
+      mint: 'rgb(43, 193, 188)',
+    },
+  },
   utils: {
-    flexCenter: (value = 'row') => ({
+    textHightLight: () => ({
+      display: 'inline-block',
+      position: 'relative',
+      fontWeight: '700',
+      fontSize: '16px',
+
+      '&::after': {
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+        height: '8px',
+        backgroundColor: '$mint',
+        opacity: '0.5',
+        zIndex: '-1',
+      },
+    }),
+    flexCenter: (value: string) => ({
       display: 'flex',
-      flexDirection: value,
+      flexDirection: value || 'row',
       alignItems: 'center',
       justifyContent: 'center',
     }),
