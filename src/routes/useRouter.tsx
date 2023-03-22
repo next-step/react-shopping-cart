beded696d6b9c4787263ba4ba7c6c0ea5ed6489a
dahye1013 <alrean@naver.com>
2023-03-23 00:17:50 +0900

    chore: router back 함수 default parameter 세팅

--- src/routes/useRouter.tsx
+++ src/routes/useRouter.tsx
@@ -6,8 +6,8 @@ import type * as Route from '@/routes/Route';
 export const useRouter = () => {
   const navigate = useNavigate();
 
-  const back = useCallback(() => {
-    navigate(-1);
+  const back = useCallback((delta = -1) => {
+    navigate(delta);
   }, [navigate]);
 
   const go = useCallback(
