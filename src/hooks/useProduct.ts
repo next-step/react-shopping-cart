9a7f6ceb31a8fbc6472c687f727acfca181cda55
seyoungjoy <diddpwl80@gmail.com>
2023-03-23 00:26:09 +0900

    refactor : useProduct mutation 코드 추가

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,25 +1,40 @@
 import useCustomQuery from './useCustomQuery';
-import { ProductType } from '../types';
+import { CartItemType, ProductType } from '../types';
 import { useCallback } from 'react';
 import { ROUTE } from '../router';
 import { useNavigate } from 'react-router-dom';
+import { useCustomMutation, useRouter } from './index';
+import { updateCartList } from '../api/request';
+import { CONFIRM } from '../constant/message';
 interface UseProductResponse {
   response: ProductType[];
 }
 
 const useProduct = () => {
   const navigate = useNavigate();
+  const { confirmAndRoute } = useRouter();
   const { data, isLoading, error } = useCustomQuery<UseProductResponse>(
     `${process.env.REACT_APP_API_PATH}/products`
   );
-
-  const products = data?.response;
+  const { mutate } = useCustomMutation<unknown, CartItemType>((payload) =>
+    updateCartList(payload)
+  );
+  const addCart = async (item: ProductType) => {
+    await mutate({ id: Number(item.id), product: item });
+    confirmAndRoute(CONFIRM.CART_AND_ROUTE, ROUTE.CART);
+  };
 
   const navigateToDetailedPage = useCallback((id: number | undefined) => {
     navigate(`${ROUTE.DETAIL}/${id}`);
   }, []);
 
-  return { products, isLoading, error, navigateToDetailedPage };
+  return {
+    products: data?.response,
+    isLoading,
+    error,
+    navigateToDetailedPage,
+    addCart,
+  };
 };
 
 export default useProduct;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,11 @@
 import Layout from '../layout/Layout';
 import SectionProductList from '../components/home/SectionProductList';
-import { useRouter, useProduct, useCustomMutation } from '../hooks';
-import { CartItemType, ProductType } from '../types';
-import { CONFIRM } from '../constant/message';
-import { ROUTE } from '../router';
-import { updateCartList } from '../api/request';
+import { useProduct } from '../hooks';
+import { ProductType } from '../types';
 
 const Home = () => {
-  const { confirmAndRoute } = useRouter();
-  const { products, error, isLoading, navigateToDetailedPage } = useProduct();
-  const { mutate } = useCustomMutation<unknown, CartItemType>((payload) =>
-    updateCartList(payload)
-  );
-  const addCart = async (item: ProductType) => {
-    await mutate({ id: Number(item.id), product: item });
-    confirmAndRoute(CONFIRM.CART_AND_ROUTE, ROUTE.CART);
-  };
+  const { products, error, isLoading, navigateToDetailedPage, addCart } =
+    useProduct();
 
   if (isLoading) {
     return <div>페이지 로딩 중...</div>;
