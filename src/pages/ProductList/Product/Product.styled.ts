9781f91928afa5a69e473765a136b694eaf2a19e
Woo KyungJun <natcho9010@gmail.com>
2023-03-22 16:32:14 +0900

    style: product shadow & hover animation

--- src/pages/ProductList/Product/Product.styled.ts
+++ src/pages/ProductList/Product/Product.styled.ts
@@ -2,6 +2,15 @@ import { styled } from '@/stitches.config';
 
 export const StyledProduct = styled('div', {
   cursor: 'pointer',
+  transition: 'transform .5s',
+  borderRadius: '10px',
+  overflow: 'hidden',
+  boxShadow: '0 4px 10px rgba(50,50,50,.1)',
+
+  '&:hover': {
+    transform: 'translateY(-5%)',
+    boxShadow: '0 4px 10px rgba(0,0,0,.3)',
+  },
 });
 
 export const StyledProductBottom = styled('div', {
