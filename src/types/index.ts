2c9961d6d6a352938c95fbd228ff2d0ac0839474
seyoungjoy <diddpwl80@gmail.com>
2023-03-22 15:58:19 +0900

    refactor : product event type 분리

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,11 @@
-export interface ProductType {
+export interface ProductType extends ProductEvent {
   id?: number;
   name: string;
   price: number;
   imageUrl: string;
+}
+
+export interface ProductEvent {
   onClickProductImage?: () => void;
   onClickAddCart?: () => void;
 }
