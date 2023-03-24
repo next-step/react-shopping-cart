21cc60fa3abce8790f76aaf74908a2c0167deea1
yujeong <yujeong.choung@gmail.com>
2023-03-24 19:51:10 +0900

    FEAT: ✨  Title 컴포넌트 추가

---
+++ src/components/Title.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+type StyledProps = {
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+};
+
+const Title = ({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLHeadingElement> & StyledProps) => {
+  return <H4 {...props}>{children}</H4>;
+};
+
+export default Title;
+
+const H4 = styled.h4<StyledProps>(
+  ({ color = 'black', fontSize = '12px', fontWeight = 'normal' }) => `
+  color: ${color};
+  font-weight: ${fontWeight};
+  font-size :${fontSize}
+`
+);
