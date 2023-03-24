46c8568f3766a2e4ac2e0a335699bd1bb74e4f7f
이영우 <ywlee@hanatour.com>
2023-03-24 21:00:00 +0900

    [fix] 가격 포맷팅을 toLocaleString 함수를 이용하도록 수정

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,4 @@
 export const formatPrice = (price: number | undefined) => {
   if (!price) return "0";
-  return String(price).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return price.toLocaleString();
 };
