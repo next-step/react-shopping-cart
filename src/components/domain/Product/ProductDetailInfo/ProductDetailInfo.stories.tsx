c2fa582604d18581aa28d15f2a4522263e629ec2
jlee0505 <hj960322@gmail.com>
2023-03-24 21:35:54 +0900

    feat: ProductDetailInfo 컴포넌트

--- src/components/domain/Product/ProductDetailInfo/ProductDetailInfo.stories.tsx
+++ src/components/domain/Product/ProductDetailInfo/ProductDetailInfo.stories.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ProductDetailInfo from "./ProductDetailInfo";
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { db } from "../../db/db";
+import { db } from "../../../../db/db";
+require("../../../../css/common/index.css");
 
 export default {
   title: "Components/ProductDetailInfo",
