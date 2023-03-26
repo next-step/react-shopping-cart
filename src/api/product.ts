fe24d3bb8906c95468554762539fd8dc68e11866
dahye1013 <alrean@naver.com>
2023-03-27 00:19:40 +0900

    feat: 상품 호출 페이지네이션 api 추가

--- src/api/product.ts
+++ src/api/product.ts
@@ -4,3 +4,14 @@ export const getAllProducts = async (): Promise<Product[]> => {
   const data = await request('/products', HTTP_METHOD.GET());
   return data;
 };
+
+export const getPaginatedProducts = async ({
+  start = 1,
+  end = 10,
+}): Promise<Product[]> => {
+  const data = await request(
+    `/products?_start=${start}&_end=${end}`,
+    HTTP_METHOD.GET()
+  );
+  return data;
+};
