6d0aa99dbb8546b6493c1a161da32bfaccb369b2
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-26 11:48:44 +0900

    mod: useFetch를 promise를 저장하고 throw하는 형태로 변경

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -2,14 +2,18 @@ import { useEffect, useReducer } from 'react';
 
 import { getCachedData, isOverCacheTime, setCachedData } from 'storages/memory';
 
+type Status = 'init' | 'loading' | 'pending' | 'fetched' | 'error';
+
 interface State<T> {
   data?: T;
-  isLoading: boolean;
+  status: Status;
+  promise?: Promise<void>;
   error?: Error;
 }
 
 type Action<T> =
   | { type: 'loading' }
+  | { type: 'pending'; payload: any }
   | { type: 'fetched'; payload: T }
   | { type: 'error'; payload: Error };
 
@@ -23,17 +27,21 @@ function useFetch<T = unknown>({ fetcher, cacheKey, cacheTime }: UseFetchProps<T
   const initialState: State<T> = {
     error: undefined,
     data: undefined,
-    isLoading: false,
+    status: 'init',
+    promise: undefined,
   };
 
   const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
     switch (action.type) {
       case 'loading':
-        return { ...initialState, isLoading: true };
+        return { ...initialState, status: 'loading' };
+      case 'pending': {
+        return { ...initialState, status: 'pending', promise: action.payload };
+      }
       case 'fetched':
-        return { ...initialState, data: action.payload };
+        return { ...initialState, status: 'fetched', data: action.payload };
       case 'error':
-        return { ...initialState, error: action.payload };
+        return { ...initialState, status: 'error', error: action.payload };
       default:
         return state;
     }
@@ -41,37 +49,29 @@ function useFetch<T = unknown>({ fetcher, cacheKey, cacheTime }: UseFetchProps<T
 
   const [state, dispatch] = useReducer(fetchReducer, initialState);
 
+  function resolvePromise(data: T) {
+    setCachedData({ key: cacheKey, data, cacheTime });
+    dispatch({ type: 'fetched', payload: data });
+  }
+  function rejectPromise(error: Error) {
+    dispatch({ type: 'error', payload: error });
+  }
+
   useEffect(() => {
     if (!fetcher) return;
 
     const abortController = new AbortController();
 
-    const fetchData = async () => {
-      dispatch({ type: 'loading' });
+    dispatch({ type: 'loading' });
 
-      const cachedData = getCachedData<T>(cacheKey);
+    const cachedData = getCachedData<T>(cacheKey);
 
-      if (cachedData && !isOverCacheTime(cacheKey)) {
-        dispatch({ type: 'fetched', payload: cachedData });
-        return;
-      }
+    if (cachedData && !isOverCacheTime(cacheKey)) {
+      dispatch({ type: 'fetched', payload: cachedData });
+      return;
+    }
 
-      try {
-        const data = await fetcher();
-
-        setCachedData({ key: cacheKey, data, cacheTime });
-
-        dispatch({ type: 'fetched', payload: data });
-      } catch (error: unknown) {
-        if (isError(error) && error.name === 'AbortError') {
-          return;
-        }
-
-        dispatch({ type: 'error', payload: error as Error });
-      }
-    };
-
-    fetchData();
+    dispatch({ type: 'pending', payload: fetcher().then(resolvePromise, rejectPromise) });
 
     return () => {
       abortController.abort();
@@ -80,9 +80,15 @@ function useFetch<T = unknown>({ fetcher, cacheKey, cacheTime }: UseFetchProps<T
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetcher]);
 
+  if (state.status === 'pending' && state.promise) {
+    throw state.promise;
+  }
+
+  if (state.status === 'error') {
+    throw state.error;
+  }
+
   return state;
 }
-
-const isError = (args: any): args is Error => args !== undefined;
 
 export default useFetch;
