ee3a78d97b5b4316c6844d41622667bbd3a2b900
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-28 00:43:01 +0900

    feat: Cart store 및 store action 테스트 코드 추가

---
+++ client/src/__test__/store/cart.test.ts
@@ -0,0 +1,75 @@
+import { useCartStore } from 'store/cart';
+
+import { Carts } from 'types/cart';
+
+const id = 1679917164549;
+const INITIAL_STATE: Carts = [
+  {
+    count: 1,
+    id,
+    product: {
+      id: 4,
+      imageUrl:
+        'https://cdn-mart.baemin.com/sellergoods/main/ff297e50-7d6b-4a87-8509-b387442ff877.jpg',
+      name: '리치스 스위트콘 대 2.95kg',
+      price: 4700,
+    },
+  },
+];
+
+describe('Cart Store', () => {
+  beforeEach(() => {
+    const {
+      actions: { initialize },
+    } = useCartStore.getState();
+
+    initialize(INITIAL_STATE);
+  });
+
+  test('장바구니 목록 중 특정 항목의 수량을 늘릴 수 있어야 한다.', () => {
+    const {
+      actions: { increase },
+    } = useCartStore.getState();
+
+    increase(id);
+
+    expect(useCartStore.getState().carts[0].count).toBe(2);
+  });
+
+  test('장바구니 목록 중 특정 항목의 수량을 줄일 수 있어야 한다.', () => {
+    const {
+      actions: { decrease },
+    } = useCartStore.getState();
+
+    decrease(id);
+
+    expect(useCartStore.getState().carts[0].count).toBe(0);
+  });
+
+  test('장바구니 목록 중 특정 항목을 체크 또는 체크 해제할 수 있어야 한다.', () => {
+    const {
+      actions: { toggle },
+    } = useCartStore.getState();
+
+    toggle(id);
+    expect(useCartStore.getState().carts[0].isChecked).toBe(true);
+
+    toggle(id);
+    expect(useCartStore.getState().carts[0].isChecked).toBe(false);
+  });
+
+  test('장바구니 목록 전체 항목을 체크 또는 체크 해제할 수 있어야 한다.', () => {
+    const {
+      actions: { checkAll, uncheckAll, initialize },
+    } = useCartStore.getState();
+    initialize([...INITIAL_STATE, ...INITIAL_STATE]);
+
+    checkAll();
+    expect(useCartStore.getState().carts[0].isChecked).toBe(true);
+    expect(useCartStore.getState().carts[1].isChecked).toBe(true);
+
+    uncheckAll();
+    expect(useCartStore.getState().carts[0].isChecked).toBe(false);
+    expect(useCartStore.getState().carts[1].isChecked).toBe(false);
+  });
+});
---
+++ client/src/store/cart/index.ts
@@ -0,0 +1,55 @@
+import { create } from 'zustand';
+
+import {
+  checkAllCart,
+  decreaseCartCount,
+  increaseCartCount,
+  initializeCarts,
+  isCheckedAll,
+  isCheckedCart,
+  toggleCart,
+  totalCountOfCheckedCarts,
+  totalPriceOfCheckedCarts,
+  uncheckAllCart,
+} from './model';
+
+import { CheckableCart } from './types';
+import { Carts } from 'types/cart';
+
+type State = {
+  carts: CheckableCart[];
+};
+
+type Action = {
+  actions: {
+    initialize: (carts: Carts) => void;
+    increase: (id: number) => void;
+    decrease: (id: number) => void;
+    toggle: (id: number) => void;
+    checkAll: () => void;
+    uncheckAll: () => void;
+  };
+};
+
+export const useCartStore = create<State & Action>()((set) => ({
+  carts: [],
+  actions: {
+    initialize: (carts: Carts) => set(() => ({ carts: initializeCarts(carts) })),
+    increase: (id: number) => set((state) => ({ carts: increaseCartCount(state.carts, id) })),
+    decrease: (id: number) => set((state) => ({ carts: decreaseCartCount(state.carts, id) })),
+    toggle: (id: number) => set((state) => ({ carts: toggleCart(state.carts, id) })),
+    checkAll: () => set((state) => ({ carts: checkAllCart(state.carts) })),
+    uncheckAll: () => set((state) => ({ carts: uncheckAllCart(state.carts) })),
+  },
+}));
+
+export const useCartActions = () => useCartStore((state) => state.actions);
+export const useCartsState = () => useCartStore((state) => state.carts);
+
+export const useIsCheckedCart = (id: number) =>
+  useCartStore((state) => isCheckedCart(state.carts, id));
+export const useIsCheckedAll = () => useCartStore((state) => isCheckedAll(state.carts));
+export const useTotalPriceOfCheckedCarts = () =>
+  useCartStore((state) => totalPriceOfCheckedCarts(state.carts));
+export const useTotalCountOfCheckedCarts = () =>
+  useCartStore((state) => totalCountOfCheckedCarts(state.carts));
