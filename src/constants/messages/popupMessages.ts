8649335b2d900eb0f4cd27e1947a9beb25751e52
ag502 <ag502@naver.com>
2023-03-28 22:00:33 +0900

    add: popup confirm 메세지 상수 추가

---
+++ src/constants/messages/popupMessages.ts
@@ -0,0 +1,6 @@
+export const CONFIRM_MESSAGES = {
+  MULTIPLE_DELETE_CONFIRM_MESSAGE(numOfItems: number) {
+    return `${numOfItems}개의 장바구니 항목을 삭제하시겠습니까?`;
+  },
+  DELETE_CONFIRM_MESSAGE: "해당 장바구니 항목을 삭제하시겠습니까?",
+};
