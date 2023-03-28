12760ff9532d1386911ac04cac77b9ab8d28a129
areumsheep <areumsheep@gmail.com>
2023-03-29 02:53:01 +0900

    Add: 공통 컴포넌트 한 번에 사용하도록 export 문 추가

---
+++ src/components/@common/index.ts
@@ -0,0 +1,5 @@
+export { Typography } from './Typography';
+export { Button } from './Button';
+export { Divider } from './Divider';
+export { Header } from '../Header';
+export { Box } from './Box';
