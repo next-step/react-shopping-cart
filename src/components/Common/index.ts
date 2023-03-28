113122a15ec28ced6a8e7730c1964908ecf77ffe
Leebonggu <leebonggu12@hanmail.net>
2023-03-28 18:52:42 +0900

    refactor: import 정리

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -2,5 +2,7 @@ export { default as Heading } from './Heading';
 export { default as Button } from './Button';
 export { default as Image } from './Image';
 export { default as NumberInput } from './NumberInput';
+export { default as Checkbox } from './Checkbox';
 
 export { default as TableRow } from './TableRow';
+export { default as Summary } from './Summary';
