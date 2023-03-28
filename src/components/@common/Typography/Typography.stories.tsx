b70c68c5c1b73f313b8af6aef8fd828e27deb637
areumsheep <areumsheep@gmail.com>
2023-03-28 17:12:46 +0900

    Add: Typography 컴포넌트 및 스토리북 추가

---
+++ src/components/@common/Typography/Typography.stories.tsx
@@ -0,0 +1,61 @@
+import type { ComponentStory, Story } from '@storybook/react';
+
+import Typography from './Typography';
+import { variants } from './Typography.types';
+
+export default {
+  title: 'Common/Typography',
+  component: Typography,
+  argTypes: {
+    as: {
+      table: {
+        disable: true,
+      },
+    },
+    variant: {
+      control: { type: 'radio', options: Object.keys(variants) },
+    },
+  },
+};
+
+export const Primary: ComponentStory<typeof Typography> = (args) => (
+  <Typography {...args}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+    tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
+    doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi
+    quidem quibusdam.
+  </Typography>
+);
+
+Primary.args = {
+  variant: 'body1',
+  color: 'black',
+};
+
+export const Overview: Story<{}> = () => (
+  <>
+    <Typography variant="h1">h1. Heading</Typography>
+    <Typography variant="h2">h2. Heading</Typography>
+    <Typography variant="h3">h3. Heading</Typography>
+    <Typography variant="h4">h4. Heading</Typography>
+    <Typography variant="h5">h5. Heading</Typography>
+    <Typography variant="body1">
+      body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+      blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+      neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
+      Eum quasi quidem quibusdam.{' '}
+    </Typography>
+    <Typography variant="body2">
+      body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+      blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+      neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
+      Eum quasi quidem quibusdam.{' '}
+    </Typography>
+    <Typography variant="subtitle1">
+      subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+      blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+      neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
+      Eum quasi quidem quibusdam.{' '}
+    </Typography>
+  </>
+);
---
+++ src/components/@common/Typography/Typography.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { variants, TypographyProps } from './Typography.types';
+import * as Styled from './Typography.styles';
+
+const Typography = React.forwardRef<
+  HTMLHeadingElement | HTMLParagraphElement | HTMLSpanElement,
+  TypographyProps
+>(
+  (
+    {
+      as,
+      color = 'black',
+      variant = 'body1',
+      lineHeight,
+      children,
+      ...attributes
+    },
+    ref
+  ) => {
+    return (
+      <Styled.Typography
+        as={as || variants[variant].as}
+        color={color}
+        variant={variant}
+        lineHeight={lineHeight}
+        ref={ref}
+        {...attributes}
+      >
+        {children}
+      </Styled.Typography>
+    );
+  }
+);
+
+export default Typography;
