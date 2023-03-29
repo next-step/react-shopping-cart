014e02e2eca6565ec84cf2532977ce13746607aa
areumsheep <areumsheep@gmail.com>
2023-03-30 05:23:13 +0900

    Add: httpRequest 유틸함수 추가

---
+++ src/utils/http.ts
@@ -0,0 +1,26 @@
+import type { APIPath } from 'constants/api';
+
+export interface RequestOptions {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+}
+
+async function httpRequest<T>(
+  url: APIPath,
+  options: Partial<RequestOptions> = {}
+): Promise<T> {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const responseData = (await response.json()) as T;
+    return responseData;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+export default httpRequest;
