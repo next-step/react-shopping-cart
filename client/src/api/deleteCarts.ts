55a0b965fd211b1d43fc624ed9a2d1c03650a890
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-03-29 22:14:37 +0900

    feat: 장바구니 삭제를 위한 mutation 함수 추가

---
+++ client/src/api/deleteCarts.ts
@@ -0,0 +1,17 @@
+import { API } from 'constants/api';
+import { CartsSchema } from 'types/cart';
+
+export default async function deleteCarts(params: number[]) {
+  const response = await fetch(API.CART, {
+    method: 'DELETE',
+    body: JSON.stringify(params),
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+
+  return CartsSchema.parse(data);
+}
--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,4 @@
 export { default as fetchProducts } from './fetchProducts';
 export { default as fetchCarts } from './fetchCarts';
 export { default as addCart } from './addCart';
+export { default as deleteCarts } from './deleteCarts';
