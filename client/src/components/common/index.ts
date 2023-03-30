7a6363da8d2d570b0b9d6c997417946307e094a6
TheGiantKiller <ktsdsd94@gmail.com>
2023-03-30 21:59:05 +0900

    feat : Spinner 컴포넌트 생성

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -4,3 +4,4 @@ export { HorizontalLine } from './HorizontalLine';
 export { Input } from './Input';
 export { PageHeader } from './PageHeader';
 export { NavBar } from './NavBar';
+export { Spinner } from './Spinner';
--- client/src/pages/ProductList/ProductListPage.tsx
+++ client/src/pages/ProductList/ProductListPage.tsx
@@ -3,13 +3,15 @@ import * as Styled from './ProductListPage.styles';
 import uuid from 'react-uuid';
 
 import ProductItem from 'components/domain/Product/Item';
+import { Spinner } from 'components/common';
+
 import useProducts from 'hooks/useProductList';
 
 const ProductListPage = () => {
   const { products, status } = useProducts();
 
   if (status === 'Loading') {
-    return <div>로딩중...</div>;
+    return <Spinner />;
   }
 
   return (
