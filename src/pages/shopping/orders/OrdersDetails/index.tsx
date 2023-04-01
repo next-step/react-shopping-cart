ee753612c4409051b1a514e1037773d88149e72c
yujeong <yujeong.choung@gmail.com>
2023-04-01 19:33:33 +0900

    FEAT: ✨  주문 가격 정보 추가

--- src/pages/shopping/orders/OrdersDetails/index.tsx
+++ src/pages/shopping/orders/OrdersDetails/index.tsx
@@ -3,6 +3,7 @@ import Loading from 'components/Loading';
 import PageContainer from 'components/PageContainer';
 import { ORDERS } from 'constants/orders';
 import useAxios from 'hooks/useAxios';
+import Checkout from 'pages/shopping/carts/Checkout';
 import PageTitle from 'pages/shopping/components/PageTitle';
 import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
@@ -24,21 +25,29 @@ const OrdersDetails = () => {
 
   return (
     <PageContainer>
-      <FlexContainer direction="column" justifyContent="center">
-        <PageTitle
-          titleColor={colors.purple}
-          horizontalBarColor={colors.purple}
-        >
-          주문내역상세
-        </PageTitle>
+      <FlexContainer direction="column">
+        <FlexContainer direction="column" justifyContent="center">
+          <PageTitle
+            titleColor={colors.purple}
+            horizontalBarColor={colors.purple}
+          >
+            주문내역상세
+          </PageTitle>
 
-        <FlexContainer margin="40px 0">
-          <OrdersContainer orderNum={Number(id)} isListPage={false}>
-            {data?.orderDetails?.map((item: CartItem) => {
-              return <OrderItem item={item} key={item.id} />;
-            })}
-          </OrdersContainer>
+          <FlexContainer margin="40px 0">
+            <OrdersContainer orderNum={Number(id)} isListPage={false}>
+              {data?.orderDetails?.map((item: CartItem) => {
+                return <OrderItem item={item} key={item.id} />;
+              })}
+            </OrdersContainer>
+          </FlexContainer>
         </FlexContainer>
+
+        <Checkout
+          total={0}
+          flexContainerStyles={{ alignSelf: 'flex-end', flex: '1 0 50%' }}
+          isOrderPage
+        />
       </FlexContainer>
     </PageContainer>
   );
