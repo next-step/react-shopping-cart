8f5e87c1befb39ada078f03320246612bb81721c
yujeong <yujeong.choung@gmail.com>
2023-04-01 15:31:23 +0900

    FEAT: ✨  carts, orders 핸들러 추가

--- src/mocks/handlers/carts.ts
+++ src/mocks/handlers/carts.ts
@@ -5,8 +5,21 @@ import cartsData from 'mocks/data/carts.json';
 
 const handlers = [
   // 카트 리스트 가져오기
-  rest.get<Cart[]>(`/${CARTS}`, (req, res, ctx) => {
+  rest.get<Cart[]>(`/${CARTS}`, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json<Cart[]>(cartsData.carts));
+  }),
+  // 카트 아이템 추가
+  rest.post<Cart[]>(`/${CARTS}`, async (req, res, ctx) => {
+    const selectedCart: Cart = await req.json();
+
+    const selected: Cart | undefined = cartsData.carts.find(
+      (cart) => cart.id === selectedCart.id
+    );
+    return res(ctx.status(201), ctx.json<Cart | undefined>(selected));
+  }),
+  // 카트 아이템 삭제
+  rest.delete(`/${CARTS}/:id`, (req, res, ctx) => {
+    return res(ctx.status(204));
   }),
 ];
 export default handlers;
--- src/mocks/handlers/orders.ts
+++ src/mocks/handlers/orders.ts
@@ -8,5 +8,17 @@ const handlers = [
   rest.get<Order[]>(`/${ORDERS}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json<Order[]>(ordersData.orders));
   }),
+
+  // 특정 주문내역 가져오기
+  rest.get<Order>(`/${ORDERS}/:id`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json<Order | undefined>(
+        ordersData.orders.find(
+          (order: Order) => order.id === parseInt(req.params.id as string)
+        )
+      )
+    );
+  }),
 ];
 export default handlers;
