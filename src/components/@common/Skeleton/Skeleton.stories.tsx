2e9ef9761dd4da2e9274781d4fd65a588cf4536b
areumsheep <areumsheep@gmail.com>
2023-04-01 17:51:25 +0900

    Update: Skeleton 스토리북 Variant 추가

--- src/components/@common/Skeleton/Skeleton.stories.tsx
+++ src/components/@common/Skeleton/Skeleton.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, ComponentStory } from '@storybook/react';
+import { Box } from '../Box';
+import { Typography } from '../Typography';
 
 import Skeleton from './Skeleton';
 import { SkeletonVariant } from './Skeleton.types';
@@ -24,3 +26,26 @@ Primary.args = {
   animation: 'pulse',
   variant: 'rounded',
 };
+
+export const Variant: ComponentStory<typeof Skeleton> = (args) => (
+  <Box display="flex" gap="10">
+    <div>
+      <Typography variant="body1" className="mb-5">
+        circular
+      </Typography>
+      <Skeleton width={100} height={100} variant="circular" />
+    </div>
+    <div>
+      <Typography variant="body1" className="mb-5">
+        rectangular
+      </Typography>
+      <Skeleton width={100} height={100} variant="rectangular" />
+    </div>
+    <div>
+      <Typography variant="body1" className="mb-5">
+        rounded
+      </Typography>
+      <Skeleton width={100} height={100} variant="rounded" />
+    </div>
+  </Box>
+);
