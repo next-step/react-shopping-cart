2946d57256bf01e8de3d8dacf8c3a72b2daf25c2
areumsheep <areumsheep@gmail.com>
2023-04-01 17:41:54 +0900

    Update: Typography 스토리북 height 값 수정

--- src/components/@common/Typography/Typography.stories.tsx
+++ src/components/@common/Typography/Typography.stories.tsx
@@ -39,19 +39,19 @@ export const Overview: Story<{}> = () => (
     <Typography variant="h3">h3. Heading</Typography>
     <Typography variant="h4">h4. Heading</Typography>
     <Typography variant="h5">h5. Heading</Typography>
-    <Typography variant="body1">
+    <Typography variant="body1" height="auto">
       body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
       blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
       neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
       Eum quasi quidem quibusdam.{' '}
     </Typography>
-    <Typography variant="body2">
+    <Typography variant="body2" height="auto">
       body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
       blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
       neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
       Eum quasi quidem quibusdam.{' '}
     </Typography>
-    <Typography variant="subtitle1">
+    <Typography variant="subtitle1" height="auto">
       subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
       blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
       neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti?
