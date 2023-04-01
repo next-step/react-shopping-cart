1ae95b7993e9b2ded636e03111ca0aafeb08d6cf
areumsheep <areumsheep@gmail.com>
2023-04-01 17:53:36 +0900

    Update: Skeleton 스토리북 Animation 추가

--- src/components/@common/Skeleton/Skeleton.stories.tsx
+++ src/components/@common/Skeleton/Skeleton.stories.tsx
@@ -27,7 +27,7 @@ Primary.args = {
   variant: 'rounded',
 };
 
-export const Variant: ComponentStory<typeof Skeleton> = (args) => (
+export const Variant: ComponentStory<typeof Skeleton> = () => (
   <Box display="flex" gap="10">
     <div>
       <Typography variant="body1" className="mb-5">
@@ -46,6 +46,29 @@ export const Variant: ComponentStory<typeof Skeleton> = (args) => (
         rounded
       </Typography>
       <Skeleton width={100} height={100} variant="rounded" />
+    </div>
+  </Box>
+);
+
+export const Animation: ComponentStory<typeof Skeleton> = () => (
+  <Box display="flex" gap="10">
+    <div>
+      <Typography variant="body1" className="mb-5">
+        false
+      </Typography>
+      <Skeleton width={170} height={100} variant="rounded" />
+    </div>
+    <div>
+      <Typography variant="body1" className="mb-5">
+        pulse
+      </Typography>
+      <Skeleton width={170} height={100} variant="rounded" animation="pulse" />
+    </div>
+    <div>
+      <Typography variant="body1" className="mb-5">
+        wave
+      </Typography>
+      <Skeleton width={170} height={100} variant="rounded" animation="wave" />
     </div>
   </Box>
 );
