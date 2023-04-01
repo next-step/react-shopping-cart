0959f99463fd216dfac5c0ba3401f16dde450a60
areumsheep <areumsheep@gmail.com>
2023-04-01 17:42:48 +0900

    Rename: Layout 파일 위치 변경

--- src/components/@common/index.ts
+++ src/components/@common/index.ts
@@ -2,5 +2,6 @@ export { Typography } from './Typography';
 export { Button } from './Button';
 export { Divider } from './Divider';
 export { Box } from './Box';
-export { Layout } from './Layout';
 export { Skeleton } from './Skeleton';
+
+export { default as Layout } from './Layout';
