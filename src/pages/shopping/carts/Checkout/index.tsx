f84050ecc8e9c9c55001a5c224e37f6ec15851f8
yujeong <yujeong.choung@gmail.com>
2023-04-01 19:40:40 +0900

    FEAT: ✨  Checkout컴포넌트에 분기 추가

--- src/pages/shopping/carts/Checkout/index.tsx
+++ src/pages/shopping/carts/Checkout/index.tsx
@@ -1,22 +1,31 @@
-import FlexContainer from 'components/FlexContainer';
+import FlexContainer, { FlexContainerProps } from 'components/FlexContainer';
 import Title from 'components/Title';
 import { PropsWithChildren } from 'react';
 import * as StyledCheckout from './Checkout.styled';
 
 interface CheckoutProps extends PropsWithChildren {
   total: number;
+  flexContainerStyles?: FlexContainerProps;
+  isOrderPage?: boolean;
 }
 
-const Checkout = ({ total, children }: CheckoutProps) => {
+const Checkout = ({
+  total,
+  children,
+  flexContainerStyles,
+  isOrderPage,
+}: CheckoutProps) => {
   return (
-    <FlexContainer direction="column" flex="1">
+    <FlexContainer direction="column" flex="1" {...flexContainerStyles}>
       <StyledCheckout.FirstBorderBox>
-        <Title fontSize="20px">결제예상금액</Title>
+        <Title fontSize="20px">
+          {isOrderPage ? ' 결제금액 정보' : '결제예상금액'}
+        </Title>
       </StyledCheckout.FirstBorderBox>
       <StyledCheckout.SecondBorderBox>
-        <FlexContainer justifyContent="space-between">
+        <FlexContainer justifyContent="space-between" gap={'10px'}>
           <Title highlight fontSize="20px">
-            결제예상금액
+            {isOrderPage ? ' 총 결제금액' : '결제예상금액'}
           </Title>
           <Title highlight fontSize="20px">
             {total}원
