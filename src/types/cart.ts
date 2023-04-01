fea0eec8904b0988fd2183bdd907b42543717fac
yujeong <yujeong.choung@gmail.com>
2023-04-01 15:38:38 +0900

    FEAT: ✨  cart type 변경

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,6 +1,9 @@
 import { Product } from 'types/products';
 
+export interface CartItem extends Product {
+  quantity: number;
+}
 export interface Cart {
   id: number;
-  product: Product;
+  product: CartItem;
 }
