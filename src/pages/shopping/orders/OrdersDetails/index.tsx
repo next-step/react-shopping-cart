1223b2d508ba4cf5a3f8fa4e337a54d543a21999
yujeong <yujeong.choung@gmail.com>
2023-04-01 15:47:40 +0900

    FEAT: ✨  orderdetail 페이지 추가

--- src/pages/shopping/orders/OrdersDetails/index.tsx
+++ src/pages/shopping/orders/OrdersDetails/index.tsx
@@ -1,5 +1,42 @@
+import FlexContainer from 'components/FlexContainer';
+import PageContainer from 'components/PageContainer';
+import { ORDERS } from 'constants/orders';
+import useAxios from 'hooks/useAxios';
+import PageTitle from 'pages/shopping/components/PageTitle';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ThemeContext } from 'styled-components';
+import { CartItem } from 'types/cart';
+import { Order } from 'types/order';
+import OrderItem from '../OrderItem';
+import OrdersContainer from '../OrdersContainer';
+
 const OrdersDetails = () => {
-  return <div></div>;
+  const { colors } = useContext(ThemeContext);
+
+  const { id } = useParams();
+  const { isLoading, data } = useAxios<Order>({ url: `/${ORDERS}/${id}` });
+
+  return (
+    <PageContainer>
+      <FlexContainer direction="column" justifyContent="center">
+        <PageTitle
+          titleColor={colors.purple}
+          horizontalBarColor={colors.purple}
+        >
+          주문내역상세
+        </PageTitle>
+
+        <FlexContainer margin="40px 0">
+          <OrdersContainer orderNum={Number(id)} isListPage={false}>
+            {data?.orderDetails?.map((item: CartItem) => {
+              return <OrderItem item={item} key={item.id} />;
+            })}
+          </OrdersContainer>
+        </FlexContainer>
+      </FlexContainer>
+    </PageContainer>
+  );
 };
 
 export default OrdersDetails;
