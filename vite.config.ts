6f27748aacc417b3401f4f678fd0ea5615883c3b
Leebonggu <leebonggu12@hanmail.net>
2023-04-05 00:54:14 +0900

    refactor: test settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
