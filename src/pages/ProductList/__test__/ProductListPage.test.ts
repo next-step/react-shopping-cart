c5c433f64f14d3b14cf1942e86dd444ebdce654b
Leebonggu <leebonggu12@hanmail.net>
2023-04-05 00:49:38 +0900

    feat: 테스트 코드 예시

--- src/pages/ProductList/__test__/ProductListPage.test.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@/mocks/factory';
-
-describe('productPage를 테스트 합니다', () => {
-  test('factory test', () => {
-    const data = [db.products.create(), db.products.create(), db.products.create(), db.products.create()];
-
-    expect(data.length).toBe(4);
-  });
-});
