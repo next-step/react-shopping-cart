07f40f91897d73d0ee2d5b4685815f64449702f8
realhee <wlsgml5628@gmail.com>
2023-04-05 16:52:46 +0900

    Refactor: CRA 관련 불필요한 파일 정리

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App.js";
 import { worker } from "./mocks/worker.js";
+
 if (process.env.NODE_ENV === "development") {
   worker.start();
 }
