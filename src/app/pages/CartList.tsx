4dc2b813dec421a04f83744ff326d398a0a8cec3
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2023-04-07 05:24:57 +0900

    feat: cart 상품 선택 기능, 예상 주문 정보 계산 기능 추가

--- src/app/pages/CartList.tsx
+++ src/app/pages/CartList.tsx
@@ -1,21 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect, ReactEventHandler, ChangeEventHandler } from 'react';
 import { ICartTypes } from '../../@interface';
 import { getAllCarts } from '../../api/cart';
 import { LOCALSTORAGE } from '../../constants/dataKey';
 import { getJSONData, setJSONData } from '../../utils/localStorage';
 
-interface ICartListProp {
-  handleClick: React.MouseEventHandler;
-}
+interface ICartListProp {}
 
 const CartList = () => {
-  const [data, setData] = useState(getJSONData(LOCALSTORAGE.CART));
-  if (data.length === 0) {
+  const [data, setData] = useState<ICartTypes[]>(getJSONData(LOCALSTORAGE.CART));
+  const [checked, setChecked] = useState<Array<number>>([]);
+  const [cost, setCost] = useState(0);
+
+  useEffect(() => {
     getAllCarts().then((res) => {
       setJSONData(LOCALSTORAGE.CART, res);
       setData(res);
     });
-  }
+  }, []);
+
+  useEffect(() => {
+    let newCost = 0;
+    data.forEach((item) => {
+      if (checked.includes(item.id)) newCost += item.product.price;
+    });
+    setCost(newCost);
+  }, [checked, data]);
+
+  const handleChecked = (evt: React.ChangeEvent, id: number) => {
+    const nextState = checked.includes(id) ? checked.filter((value: number) => value !== id) : [...checked, id];
+    setChecked((prev) => nextState);
+  };
+
+  const handleAllChecked = (evt: React.ChangeEvent) => {
+    if (checked.length > 0) setChecked([]);
+  };
+
   return (
     <main className="cart-section">
       <header className="flex-col-center mt-20">
@@ -27,7 +46,13 @@ const CartList = () => {
         <section className="cart-left-section">
           <div className="flex justify-between items-center">
             <div className="checkbox-container">
-              <input className="checkbox" name="checkbox" type="checkbox" />
+              <input
+                className="checkbox"
+                name="checkbox"
+                type="checkbox"
+                onChange={handleAllChecked}
+                checked={checked.length > 0}
+              />
               <label className="checkbox-label" htmlFor="checkbox">
                 선택해제
               </label>
@@ -36,11 +61,17 @@ const CartList = () => {
           </div>
           <h3 className="cart-title">든든배송 상품({data.length}개)</h3>
           <hr className="divide-line-gray mt-10" />
-          {data.map(({ product }: ICartTypes, idx: number) => (
-            <>
-              <div className="cart-container" key={idx}>
+          {data.map(({ product, id }: ICartTypes) => (
+            <div key={id}>
+              <div className="cart-container">
                 <div className="flex gap-15 mt-10">
-                  <input className="checkbox" name="checkbox" type="checkbox" />
+                  <input
+                    className="checkbox"
+                    name="checkbox"
+                    type="checkbox"
+                    checked={checked.includes(id)}
+                    onChange={(evt) => handleChecked(evt, id)}
+                  />
                   <img className="w-144 h-144" src={product.imageUrl} alt={product.name} />
                   <span className="cart-name">{product.name}</span>
                 </div>
@@ -57,7 +88,7 @@ const CartList = () => {
                 </div>
               </div>
               <hr className="divide-line-thin mt-10" />
-            </>
+            </div>
           ))}
         </section>
         <section className="cart-right-section">
@@ -68,10 +99,10 @@ const CartList = () => {
           <div className="cart-right-section__bottom">
             <div className="flex justify-between p-20 mt-20">
               <span className="highlight-text">결제예상금액</span>
-              <span className="highlight-text">21,800원</span>
+              <span className="highlight-text">{cost}원</span>
             </div>
             <div className="flex-center mt-30 mx-10">
-              <button className="primary-button flex-center">주문하기(3개)</button>
+              <button className="primary-button flex-center">주문하기({checked.length}개)</button>
             </div>
           </div>
         </section>
