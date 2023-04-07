416fdbdf55a27bcb46b3791804517274a3c6ceb8
Woo KyungJun <natcho9010@gmail.com>
2023-04-07 18:42:15 +0900

    feat: add confirmModal in CartOrderPanel

--- src/pages/Cart/CartOrderPanel/CartOrderPanel.styled.ts
+++ src/pages/Cart/CartOrderPanel/CartOrderPanel.styled.ts
@@ -2,25 +2,33 @@ import { Link } from 'react-router-dom';
 
 import { styled, css } from '@/stitches.config';
 
-export const StyledCartOrderPanel = css({
-  width: '280px',
-  height: '230px',
-  marginTop: '50px',
-  border: '1px solid rgba(0,0,0,.1)',
+export const ModalBackgroundStyle = css({
+  justifyContent: 'center !important',
 });
 
-export const StyledTotalPrice = styled('div', {
-  display: 'flex',
-  justifyContent: 'space-between',
-  gap: '10px',
-  marginTop: '20px',
+export const StyledConfirmModal = styled('div', {
+  flexCenter: 'column',
+  gap: '20px',
+  width: '80vw',
+  minWidth: '350px',
+  height: '80vh',
+  minHeight: '350px',
+});
+
+export const StyledOrderList = styled('div', {
+  backgroundColor: 'White',
+  borderRadius: '20px',
+  flex: 1,
+  width: '100%',
+  overflowY: 'auto',
 });
 
 export const StyledOrderButton = styled(Link, {
   flexCenter: '',
-  flex: 1,
-  gap: '5px',
-  height: '80px',
+  width: '350px',
+  height: '100px',
   backgroundColor: '$mint',
+  borderRadius: '10px',
   color: 'White',
+  fontWeight: 'bold',
 });
--- src/pages/Cart/CartOrderPanel/CartOrderPanel.tsx
+++ src/pages/Cart/CartOrderPanel/CartOrderPanel.tsx
@@ -1,9 +1,13 @@
 import React, { MouseEvent } from 'react';
 
 import { CartSidePanel } from '@/components';
+import { OrderProductList } from '@/containers';
+import { useModal } from '@/hooks';
 import { routes } from '@/router';
 import { TCartStore } from '@/stores/CartContext';
 import { useOrderContextApiSelector } from '@/stores/OrderContext';
+
+import { ModalBackgroundStyle, StyledConfirmModal, StyledOrderButton, StyledOrderList } from './CartOrderPanel.styled';
 
 interface CartOrderPanelProps {
   cart: TCartStore;
@@ -13,31 +17,51 @@ export function CartOrderPanel({ cart }: CartOrderPanelProps) {
   const cartProducts = Object.values(cart);
 
   const orderContextApis = useOrderContextApiSelector();
+  const { Modal, showModal } = useModal();
+
+  const checkedCartProducts = cartProducts.filter((cartProduct) => cartProduct.isChecked);
 
   const handleOrderButtonClick = (e: MouseEvent<HTMLAnchorElement>) => {
-    const checkedCartProduct = cartProducts.filter((cartProduct) => cartProduct.isChecked);
-    if (!checkedCartProduct || checkedCartProduct.length <= 0) {
-      e.preventDefault();
+    e.preventDefault();
+
+    if (!checkedCartProducts || checkedCartProducts.length <= 0) {
       alert('주문하실 상품을 선택해주세요.');
       return;
     }
 
-    orderContextApis?.dispatch({ type: 'add', payload: cartProducts.filter((cartProduct) => cartProduct.isChecked) });
+    showModal();
+  };
+
+  const handleConfirmButtonClick = () => {
+    orderContextApis?.dispatch({ type: 'add', payload: checkedCartProducts });
   };
 
   return (
-    <CartSidePanel
-      cart={cart}
-      title="결제예상금액"
-      body="결제예상금액"
-      buttonContent={
-        <>
-          <span>주문하기</span>
-          <span>{`${cartProducts?.length}개`}</span>
-        </>
-      }
-      to={routes.orderList}
-      onSubmit={handleOrderButtonClick}
-    />
+    <>
+      <CartSidePanel
+        cart={cart}
+        title="결제예상금액"
+        body="결제예상금액"
+        buttonContent={
+          <>
+            <span>주문하기</span>
+            <span>{`${cartProducts?.length}개`}</span>
+          </>
+        }
+        to={routes.orderList}
+        onSubmit={handleOrderButtonClick}
+      />
+
+      <Modal className={ModalBackgroundStyle()}>
+        <StyledConfirmModal>
+          <StyledOrderList>
+            <OrderProductList order={checkedCartProducts} />
+          </StyledOrderList>
+          <StyledOrderButton to={routes.orderList} onClick={handleConfirmButtonClick}>
+            확인
+          </StyledOrderButton>
+        </StyledConfirmModal>
+      </Modal>
+    </>
   );
 }
