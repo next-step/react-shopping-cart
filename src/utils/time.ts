4cfd73f130e4cff4e24bb691e341a1619c5bfe11
Woo KyungJun <natcho9010@gmail.com>
2023-04-07 15:43:52 +0900

    feat: expose second util function

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,4 @@
-function second(second: number) {
+export function second(second: number) {
   return 1000 * second;
 }
 
@@ -7,4 +7,10 @@ export function minute(input: number) {
   const secondLong = Math.floor((input - minute) * 60);
 
   return 1000 * 60 * minute + second(secondLong);
+}
+
+export function millisecondToSecondUnit(millisecond: number) {
+  const second = (millisecond / 1000).toFixed(1);
+
+  return `${second}s`;
 }
