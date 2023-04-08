33031aa2101a9270d8933ff42599250c15d86110
realhee <wlsgml5628@gmail.com>
2023-04-09 01:49:38 +0900

    Feat: CardIcon 클릭 시 confirm 창 출력

--- src/components/product.js
+++ src/components/product.js
@@ -1,6 +1,13 @@
 import CartIcon from "../assets/svgs/cart.svg";
+import { useNavigate } from "react-router-dom";
+import { ROUTE_PATH } from "../constants/page";
 
 export default function Product({ id, name, price, imageUrl }) {
+  function handleCartClick() {
+    if (confirm("장바구니에 물건이 담겼습니다. 장바구니로 이동하시겠습니까?"))
+      useNavigate(ROUTE_PATH.CART);
+  }
+
   return (
     <div data-id={id}>
       <img src={imageUrl} alt={name} />
@@ -9,7 +16,7 @@ export default function Product({ id, name, price, imageUrl }) {
           <span className="product-info__name">{name}</span>
           <span className="product-info__price">{price}원</span>
         </div>
-        <img src={CartIcon} alt="장바구니" />
+        <img src={CartIcon} alt="장바구니" onClick={() => handleCartClick()} />
       </div>
     </div>
   );
