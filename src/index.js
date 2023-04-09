36930c5628055a9fe0ee0fb69156d116ec2afc3f
realhee <wlsgml5628@gmail.com>
2023-04-09 12:41:03 +0900

    Chore: 배포 환경에서도 Mocking data를 가져오도록 수정

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from "./App";
 import "./css/index.css";
 import { worker } from "./mocks/browser";
 
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
+//if (process.env.NODE_ENV === "development") {
+worker.start();
+//}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
