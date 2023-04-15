a21c6ed1930b50cdea5d78d272096f16ba5d4eaa
realhee <wlsgml5628@gmail.com>
2023-04-15 16:14:17 +0900

    Refactor: Fragment 사용 & map 함수 내 return 구문 생략

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,24 +1,18 @@
+import { Fragment } from "react";
 import OrderListHeader from "./OrderListHeader";
 import OrderListItem from "./OrderListItem";
 
 export default function OrderList({ orders }) {
   return (
     <div className="order-list">
-      {orders.map((order) => {
-        return (
-          <>
-            <OrderListHeader key={order.id} order={order}></OrderListHeader>
-            {order.orderDetails.map((orderDetail) => {
-              return (
-                <OrderListItem
-                  key={orderDetail.id}
-                  orderDetail={orderDetail}
-                ></OrderListItem>
-              );
-            })}
-          </>
-        );
-      })}
+      {orders.map((order) => (
+        <Fragment key={order.id}>
+          <OrderListHeader order={order}></OrderListHeader>
+          {order.orderDetails.map((orderDetail) => (
+            <OrderListItem key={orderDetail.id} orderDetail={orderDetail} />
+          ))}
+        </Fragment>
+      ))}
     </div>
   );
 }
