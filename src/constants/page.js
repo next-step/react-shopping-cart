079d52804a7936022cf87fc2defd5909f0e34f3f
realhee <wlsgml5628@gmail.com>
2023-04-15 18:14:04 +0900

    Fix: 상세 페이지 path param에 id 추가

--- src/constants/page.js
+++ src/constants/page.js
@@ -1,8 +1,8 @@
 export const ROUTE_PATH = {
   PRODUCT_LIST: "/",
-  PRODUCT_DETAIL: "/product-detail",
+  PRODUCT_DETAIL: "/product/${product_id}",
   CART: "/carts",
   ORDER: "/orders",
   PAYMENT: "/payment",
-  ORDER_DETAIL: "/order-detail",
+  ORDER_DETAIL: "/order/${order_id}",
 };
