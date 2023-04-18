26d059bedcaca87cd9d326c15ee3d9ad3b2cf787
Hojoong Choi <inegg.apps@gmail.com>
2023-04-18 09:18:40 +0900

    chore: 프로젝트 초기화 작업
    * chore: 프로젝트 초기화
    * feat: 상품 목록 페이지 마크업 구성
    * eslint
    * msw

---
+++ src/App.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="flex-col-center">
+        <nav className="nav flex justify-around">
+          <div className="flex-center">
+            <h1 className="nav-title">CLEAN CODE SHOP</h1>
+          </div>
+          <div className="flex gap-15">
+            <button className="nav-button">장바구니</button>
+            <button className="nav-button">주문목록</button>
+          </div>
+        </nav>
+      </header>
+
+      <section className="product-container">
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+        <div>
+          <img src="./assets/images/product.png" alt="PET보틀-정사각(420ml)" />
+          <div className="flex justify-between w-280 p-5">
+            <div className="product-info">
+              <span className="product-info__name">PET보틀-정사각(420ml)</span>
+              <span className="product-info__price">43,000원</span>
+            </div>
+            <img src="assets/svgs/cart.svg" alt="장바구니" />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
