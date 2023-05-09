7d9887e7cc83a01412d2de0734a01fe857de8505
jlee0505 <hj960322@gmail.com>
2023-05-09 13:22:27 +0900

    chore:  modal 디테일 수정
    - 사용자 경험을 고려한 두 버튼 위치 변경
    - 각 버튼의 기능 추가

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,32 +1,47 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 import { Product } from "../../../store/store";
 
 import Button from "../Button/Button";
-import { setIsModalOpen } from "../../../store/modalSlice";
+import { setIsModalOpen, setModalMessage } from "../../../store/modalSlice";
+import { useAppDispatch } from "../../../hooks/storeHooks";
+import { useNavigate } from "react-router-dom";
 
 export type Props = {
   type: string;
   message: string;
-  product?: Product;
+  product: Product;
 };
 
 const Modal = ({ product, type, message }: Props) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleConfirmButton = () => {
+    navigate("/cart");
+  };
+  const handleCancelButton = () => {
+    dispatch(setIsModalOpen(false));
+  };
+  useEffect(() => {
+    dispatch(setModalMessage(type));
+  }, [type, dispatch]);
+
   return (
     <div className="modal">
       <div className="modal-content">
         <p>{message}</p>
         <div className="modal-button-container">
           <Button
-            className="primary-button-small"
-            onClick={() => alert("confirm!")}
-          >
-            OK
-          </Button>
-          <Button
-            className="primary-button-small"
-            onClick={() => setIsModalOpen(false)}
+            className="primary-button primary-button-small primary-button-white"
+            onClick={handleCancelButton}
           >
             Cancel
+          </Button>
+          <Button
+            className="primary-button primary-button-small"
+            onClick={handleConfirmButton}
+          >
+            OK
           </Button>
         </div>
       </div>
