a57fe04c54424b0c9d89f85be6c6131187adcbf0
jlee0505 <hj960322@gmail.com>
2023-05-21 07:15:07 +0900

    chore: msw 배포 환경에서도 사용할 수 있게 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
-}
+// if (process.env.NODE_ENV === "development") {
+//   const { worker } = require("./mocks/browser");
+//   worker.start();
+// }
+const { worker } = require("./mocks/browser");
+worker.start();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
