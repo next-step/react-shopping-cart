71cf66904d3b434a46c200f7ca2d809a1589b8cd
jlee0505 <hj960322@gmail.com>
2023-05-21 07:41:53 +0900

    chore: msw 작업(2)

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
-function prepare() {
-  if (process.env.NODE_ENV === "development") {
-    const { worker } = require("./mocks/browser");
-    return worker.start();
-  }
-  return Promise.resolve();
-}
-
-prepare().then(() => {
-  const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-  );
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+const { worker } = require("./mocks/browser");
+worker.start({
+  onUnhandledRequest: "bypass",
 });
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
