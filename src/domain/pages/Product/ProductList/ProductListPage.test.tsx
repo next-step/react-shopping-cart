31634490f997d6ce51f9941c9724d0f982be2f00
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-21 14:59:45 +0900

    test : ProductListPage 스토리북 jest테스트 수정

--- src/domain/pages/Product/ProductList/ProductListPage.test.tsx
+++ src/domain/pages/Product/ProductList/ProductListPage.test.tsx
@@ -28,7 +28,7 @@ describe('ProductListPage 렌더링 테스트', () => {
     expect(Spinner).toBeInTheDocument();
   });
 
-  test('상품의 이미지는 8개여야 한다', async () => {
+  test('상품의 이미지,가격,이름,장바구니버튼은 8개여야 한다', async () => {
     render(<Default />);
 
     await waitFor(
@@ -38,38 +38,15 @@ describe('ProductListPage 렌더링 테스트', () => {
       },
       { timeout: 3000 }
     );
-  });
-  test('상품의 가격은 8개여야 한다', async () => {
-    render(<Default />);
+    const productsprice = screen.getAllByTestId('product-price');
+    const productsName = screen.getAllByTestId('product-name');
+    const cartButton = screen.getAllByTestId('cart-button');
 
-    await waitFor(
-      () => {
-        const productsprice = screen.getAllByTestId('product-price');
-        expect(productsprice).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
+    expect(productsprice).toHaveLength(8);
+    expect(productsName).toHaveLength(8);
+    expect(cartButton).toHaveLength(8);
   });
-  test('상품의 이름은 8개여야 한다', async () => {
-    render(<Default />);
-    await waitFor(
-      () => {
-        const productsName = screen.getAllByTestId('product-name');
-        expect(productsName).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
-  });
-  test('장바구니 버튼은 8개여야한다', async () => {
-    render(<Default />);
-    await waitFor(
-      () => {
-        const cartButton = screen.getAllByTestId('cart-button');
-        expect(cartButton).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
-  });
+
   test('ProductPage 페이지의 페이지네이션 버튼은 5개여야한다', async () => {
     render(<Default />);
     await waitFor(
@@ -82,8 +59,8 @@ describe('ProductListPage 렌더링 테스트', () => {
   });
 });
 
-describe('ProductList 페이지 네이션 동작테스트', () => {
-  test('다음 페이지 네이션 버튼을 누르면 8개의 이미지가 렌더링 되어야한다.', async () => {
+describe('ProductList 페이지 네이션 기능 테스트', () => {
+  test('다음 페이지 네이션 버튼을 누르면 8개의 이미지,가격,카트버튼, 이름이 렌더링 되어야한다.', async () => {
     render(<Default />);
     await waitFor(
       () => {
@@ -103,69 +80,12 @@ describe('ProductList 페이지 네이션 동작테스트', () => {
       },
       { timeout: 3000 }
     );
-  });
-  test('다음 페이지 네이션 버튼을 누르면 8개의 가격이 렌더링 되어야한다.', async () => {
-    render(<Default />);
-    await waitFor(
-      () => {
-        const paginationButtons = screen.getAllByTestId('pagination-button');
-        expect(paginationButtons).toHaveLength(5);
-      },
-      { timeout: 3000 }
-    );
-    const paginationButtons = screen.getAllByTestId('pagination-button');
-    const button = paginationButtons[1];
-    await userEvent.click(button);
+    const productsprice = screen.getAllByTestId('product-price');
+    const cartButton = screen.getAllByTestId('cart-button');
+    const productsName = screen.getAllByTestId('product-name');
 
-    await waitFor(
-      () => {
-        const productsprice = screen.getAllByTestId('product-price');
-        expect(productsprice).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
-  });
-  test('다음 페이지 네이션 버튼을 누르면 8개의 카트버튼이 렌더링 되어야한다.', async () => {
-    render(<Default />);
-    await waitFor(
-      () => {
-        const paginationButtons = screen.getAllByTestId('pagination-button');
-        expect(paginationButtons).toHaveLength(5);
-      },
-      { timeout: 3000 }
-    );
-    const paginationButtons = screen.getAllByTestId('pagination-button');
-    const button = paginationButtons[1];
-    await userEvent.click(button);
-
-    await waitFor(
-      () => {
-        const cartButton = screen.getAllByTestId('cart-button');
-
-        expect(cartButton).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
-  });
-  test('다음 페이지 네이션 버튼을 누르면 8개의 아이템이 렌더링 되어야한다.', async () => {
-    render(<Default />);
-    await waitFor(
-      () => {
-        const paginationButtons = screen.getAllByTestId('pagination-button');
-        expect(paginationButtons).toHaveLength(5);
-      },
-      { timeout: 3000 }
-    );
-    const paginationButtons = screen.getAllByTestId('pagination-button');
-    const button = paginationButtons[1];
-    await userEvent.click(button);
-
-    await waitFor(
-      () => {
-        const productsName = screen.getAllByTestId('product-name');
-        expect(productsName).toHaveLength(8);
-      },
-      { timeout: 3000 }
-    );
+    expect(productsName).toHaveLength(8);
+    expect(productsprice).toHaveLength(8);
+    expect(cartButton).toHaveLength(8);
   });
 });
