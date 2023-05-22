58d0629d1117b72e02212191e303dbc40149ec76
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-23 08:17:40 +0900

    test: OrderdItem 스토리북 Jest 테스트 수정

--- src/domain/components/Order/OrdredItem/OrderedItem.test.tsx
+++ src/domain/components/Order/OrdredItem/OrderedItem.test.tsx
@@ -5,25 +5,26 @@ import { composeStories } from '@storybook/react';
 import { render } from 'test/rtkProvider';
 const { Default } = composeStories(stories);
 
-describe('OrderedItem 렌더링 검증 테스트', () => {
-  test('냉면용기 (대)가 되어야 한다.', async () => {
+describe('OrderedItem 스토리북 렌더링 검증 테스트', () => {
+  test('상품이름은 냉면용기 (대)여야 한다.', async () => {
     render(<Default />);
-    const itemTitle = await screen.findByText('냉면용기(대)');
-    expect(itemTitle).toBeInTheDocument();
+    const name = await screen.findByTestId('orderItem-name');
+    expect(name).toHaveTextContent('냉면용기(대)');
   });
-  test('수량 :1이 되어야 한다', async () => {
+
+  test('상품의 수량 :1이 이어야 한다', async () => {
     render(<Default />);
-    const amount = await screen.findByText('수량:1');
-    expect(amount).toBeInTheDocument();
+    const amount = await screen.findByTestId('orderItem-amount');
+    expect(amount).toHaveTextContent('수량:1');
   });
-  test('가격:83700원이 되어야한다.', async () => {
+  test('상품의 가격은 가격:83700원이 이어야한다.', async () => {
     render(<Default />);
-    const price = await screen.findByText('가격: 83700원');
-    expect(price).toBeInTheDocument();
+    const price = await screen.findByTestId('orderItem-price');
+    expect(price).toHaveTextContent('가격: 83700원');
   });
   test('아이템 이미지가 존재해야 된다', async () => {
     render(<Default />);
-    const image = await screen.findByRole('img');
+    const image = await screen.findByTestId('orderItem-image');
     expect(image).toBeInTheDocument();
   });
 });
