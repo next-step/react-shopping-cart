83f266a05b146da756fb82fbcbf81874d32cce4c
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-22 21:20:31 +0900

    test : PageHeader 스토리북 jest Test추가

---
+++ src/common/components/PageHeader/PageHeader.test.tsx
@@ -0,0 +1,16 @@
+import * as stories from './PageHeader.stories';
+import { screen } from '@testing-library/react';
+
+import { composeStories } from '@storybook/react';
+import { render } from '../../../test/rtkProvider';
+
+const { Default } = composeStories(stories);
+
+describe('PageHeader 스토리북 렌더링 검증 테스트', () => {
+  test('Page Header 텍스트는 Page Header이어야 한다.', async () => {
+    render(<Default />);
+    const pageHeaderText = await screen.findByTestId('page-header-text');
+
+    expect(pageHeaderText).toHaveTextContent('Page Header');
+  });
+});
