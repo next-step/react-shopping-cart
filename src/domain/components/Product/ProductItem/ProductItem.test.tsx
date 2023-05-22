13f34dcc6ed1bbb0f931a02f5244da0f34c691e9
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-23 08:33:08 +0900

    test : ProductItem 스토리북 Jest test 수정

--- src/domain/components/Product/ProductItem/ProductItem.test.tsx
+++ src/domain/components/Product/ProductItem/ProductItem.test.tsx
@@ -6,24 +6,24 @@ import { render } from 'test/rtkProvider';
 const { Item } = composeStories(stories);
 
 describe('ProductItem 스토리북 렌더링 테스트', () => {
-  test('이미지가 존재 해야한다', async () => {
+  test('상품의 이미지가 존재 해야한다', async () => {
     render(<Item />);
-    const image = await screen.findByRole('img');
+    const image = await screen.findByTestId('product-image');
     expect(image).toBeInTheDocument();
   });
-  test('냉면용기(대)라는 이름이 존재해야 한다', async () => {
+  test('상품의 이름은 냉면용기(대)여야 한다.', async () => {
     render(<Item />);
-    const title = await screen.findByText('냉면용기(대)');
-    expect(title).toBeInTheDocument();
+    const name = await screen.findByTestId('product-name');
+    expect(name).toHaveTextContent('냉면용기(대)');
   });
   test('가격은 83700원 이어야한다.', async () => {
     render(<Item />);
-    const price = await screen.findByText('83700원');
-    expect(price).toBeInTheDocument();
+    const price = await screen.findByTestId('product-price');
+    expect(price).toHaveTextContent('83700');
   });
-  test('장바구니 버튼이 존재해야한다.', async () => {
+  test('장바구니 아이콘 버튼이 존재해야한다.', async () => {
     render(<Item />);
     const cartButton = await screen.findByTestId('cart-button');
-    expect(cartButton).toBeInTheDocument();
+    expect(cartButton).toHaveTextContent('cart.svg');
   });
 });
