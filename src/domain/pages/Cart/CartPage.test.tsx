7e96e8fae128ea26f3ca304fd6dc883f0134809e
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-23 17:26:06 +0900

    test : CartPage storybook jest test 수정 \

--- src/domain/pages/Cart/CartPage.test.tsx
+++ src/domain/pages/Cart/CartPage.test.tsx
@@ -20,32 +20,23 @@ afterAll(() => server.close());
 describe('장바구니 체크박스 테스트', () => {
   test('장바구니에서 첫번쨰 아이템을 선택하면 주문하기 1개,결제 예상 금액은 83700원이 되어야한다.', async () => {
     render(<Default />);
-    await waitFor(() => {
-      const checkboxs = screen.getAllByTestId('item-checkbox');
-      const item = checkboxs[0];
-      expect(item).toBeInTheDocument();
-    });
-    const firstItem = screen.getAllByTestId('item-checkbox')[0];
+
+    const itemCheckBoxs = await screen.findAllByTestId('item-checkbox');
+    const firstItem = itemCheckBoxs[0];
 
     await userEvent.click(firstItem);
 
     await waitFor(() => {
-      const paymentprice = screen.getByTestId('payment-price');
-      expect(paymentprice).toHaveTextContent('83700원');
+      const orderButton = screen.getByTestId('order-button');
+      expect(orderButton).toHaveTextContent('주문하기 1개');
     });
-    const orderButton = screen.getByTestId('order-buttton');
-    expect(orderButton).toHaveTextContent('주문하기 1개');
   });
 
   test('장바구니에서 두번쨰 아이템을 선택하면 주문하기 2개,결제 예상 금액은 112700원이 되어야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const checkboxs = screen.getAllByTestId('item-checkbox');
-      const item = checkboxs[1];
-      expect(item).toBeInTheDocument();
-    });
-    const secondItem = screen.getAllByTestId('item-checkbox')[1];
+    const itemCheckBoxs = await screen.findAllByTestId('item-checkbox');
+    const secondItem = itemCheckBoxs[1];
 
     await userEvent.click(secondItem);
 
@@ -53,79 +44,49 @@ describe('장바구니 체크박스 테스트', () => {
       const paymentprice = screen.getByTestId('payment-price');
       expect(paymentprice).toHaveTextContent('112700원');
     });
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toHaveTextContent('주문하기 2개');
   });
 
   test('장바구니에서 두번째 아이템을 해제하면 주문하기 1개,결제 예상 금액은 83700원이 되어야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const checkboxs = screen.getAllByTestId('item-checkbox');
-      const item = checkboxs[1];
-      expect(item).toBeInTheDocument();
-    });
-    const secondItem = screen.getAllByTestId('item-checkbox')[1];
+    const itemCheckBoxs = await screen.findAllByTestId('item-checkbox');
+    const secondItem = itemCheckBoxs[1];
+
     await userEvent.click(secondItem);
 
     await waitFor(() => {
       const paymentprice = screen.getByTestId('payment-price');
       expect(paymentprice).toHaveTextContent('83700원');
     });
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toHaveTextContent('주문하기 1개');
   });
 });
+
 describe('모두 선택 체크 박스 테스트', () => {
-  test('모두선택 체크 박스를 누르면 장바구니에 있는 모든 아이템들이 선택 되어야한다.', async () => {
-    render(<Default />);
-
-    await waitFor(() => {
-      const allcheckbox = screen.getByTestId('all-checkbox');
-      expect(allcheckbox).toBeInTheDocument();
-    });
-    const allcheckbox = screen.getByTestId('all-checkbox');
-    await userEvent.click(allcheckbox);
-
-    await waitFor(() => {
-      const itemscheckBox = screen.getAllByTestId('item-checkbox');
-      itemscheckBox.forEach((checkbox) => expect(checkbox).toBeChecked());
-    });
-  });
   test('모두선택 체크 박스를 누르면 주문하기 2개, 112700원이 되어야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const allcheckbox = screen.getByTestId('all-checkbox');
-      expect(allcheckbox).toBeInTheDocument();
-    });
-    const allcheckbox = screen.getByTestId('all-checkbox');
+    const allcheckbox = await screen.findByTestId('all-checkbox');
     await userEvent.click(allcheckbox);
 
     await waitFor(() => {
       const paymentprice = screen.getByTestId('payment-price');
       expect(paymentprice).toHaveTextContent('112700원');
     });
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toHaveTextContent('주문하기 2개');
   });
 
   test('모두선택 체크 박스를 다시누르면 주문하기 0개, 결제예상금액은 0원이 되어야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const allcheckbox = screen.getByTestId('all-checkbox');
-      expect(allcheckbox).toBeInTheDocument();
-    });
-
-    const allcheckbox = screen.getByTestId('all-checkbox');
+    const allcheckbox = await screen.findByTestId('all-checkbox');
     await userEvent.click(allcheckbox);
 
-    await waitFor(() => {
-      const paymentprice = screen.getByTestId('payment-price');
-      expect(paymentprice).toHaveTextContent('0원');
-    });
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toHaveTextContent('주문하기 0개');
   });
 });
@@ -133,47 +94,40 @@ describe('모두 선택 체크 박스 테스트', () => {
 describe('주문하기 버튼 테스트', () => {
   test('체크된 아이템이 존재하지 않으면 주문하기 버튼은 비활성화 되어야한다.', async () => {
     render(<Default />);
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toBeDisabled();
   });
+
   test('체크된 아이템이 존재하면 주문하기 버튼은 활성화 되어야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const checkboxs = screen.getAllByTestId('item-checkbox');
-      const item = checkboxs[0];
-      expect(item).toBeInTheDocument();
-    });
-    const firstItem = screen.getAllByTestId('item-checkbox')[0];
+    const itemCheckBoxs = await screen.findAllByTestId('item-checkbox');
+    const firstItem = itemCheckBoxs[0];
 
     await userEvent.click(firstItem);
 
-    const orderButton = screen.getByTestId('order-buttton');
+    const orderButton = screen.getByTestId('order-button');
     expect(orderButton).toBeEnabled();
   });
-  test('주문하기 버튼을 누르면 dialog가 나타나야한다.', async () => {
+
+  test('주문하기 버튼을 누르면 주문 하시겠습니까?를 가진 dialog가 나타나야한다.', async () => {
     render(<Default />);
 
-    await waitFor(() => {
-      const checkboxs = screen.getAllByTestId('item-checkbox');
-      const item = checkboxs[0];
-      expect(item).toBeInTheDocument();
-    });
-    const firstItem = screen.getAllByTestId('item-checkbox')[0];
-
+    const itemCheckBoxs = await screen.findAllByTestId('item-checkbox');
+    const firstItem = itemCheckBoxs[0];
     await userEvent.click(firstItem);
 
-    const orderButton = screen.getByTestId('order-buttton');
-
+    const orderButton = screen.getByTestId('order-button');
     await userEvent.click(orderButton);
 
     await waitFor(() => {
-      expect(screen.getByTestId('dialog')).toBeInTheDocument();
+      const dialogTitle = screen.getByTestId('dialog-title');
+      expect(dialogTitle).toHaveTextContent('주문 하시겠습니까?');
     });
   });
 });
 describe('장바구니 삭제 테스트', () => {
-  test('체크된 모든 아이템 들을 삭제 버튼을 누르고 모달창의 확인을 누르면 체크박스 아이템들이 삭제 되어야한다.', async () => {
+  test('체크된 모든 아이템 들을 삭제 버튼 누르고 모달창의 확인버튼을 누르면, 주문하기 버튼은 주문하기 0개이 되어야한다.', async () => {
     render(<Default />);
 
     const allcheckbox = await screen.findByTestId('all-checkbox');
@@ -184,11 +138,14 @@ describe('장바구니 삭제 테스트', () => {
     const cartRemoveButton = await screen.findByTestId('cart-removeButton');
     await userEvent.click(cartRemoveButton);
 
+    await waitFor(() => {
+      const dialogTitle = screen.getByTestId('dialog-title');
+      expect(dialogTitle).toHaveTextContent('상품을 삭제하시겠습니까?');
+    });
     const confirmButton = await screen.findByTestId('confirm-button');
     await userEvent.click(confirmButton);
 
-    await waitFor(() => {
-      expect(screen.queryByTestId('item-checkbox')).not.toBeInTheDocument();
-    });
+    const orderButton = await screen.findByTestId('order-button');
+    expect(orderButton).toHaveTextContent('주문하기 0개');
   });
 });
