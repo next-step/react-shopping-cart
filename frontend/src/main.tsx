ce05c06b5afa251787e1ea9fe465980bdc95efc0
Seung-wan <tmddhks0104@gmail.com>
2023-06-02 10:59:59 +0900

    Add GA (Google Analytics)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import worker from '@/mocks/server';
 
@@ -9,6 +10,10 @@ import routes from '@/routes';
 if (import.meta.env.DEV) {
   worker.start();
 }
+
+const gaTrackingId = import.meta.env.GA_TRACKING_ID;
+ReactGA.initialize(gaTrackingId, { debug: true });
+ReactGA.pageview(window.location.pathname);
 
 const router = createBrowserRouter(routes);
 
