ca21c83e61ef5acffa00106764585ae507c41406
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-22 10:28:32 +0900

    refactor : Payment test수정

--- src/domain/components/Payment/Payment.test.tsx
+++ src/domain/components/Payment/Payment.test.tsx
@@ -6,10 +6,10 @@ import { render } from 'test/rtkProvider';
 const { Default, OrderPage, CartPage } = composeStories(stories);
 
 describe('Payment 컴포넌트의 Default 테스트', () => {
-  test('1000원 Text가 존재한다', () => {
+  test('결제예상금액은 1000원이다', () => {
     render(<Default />);
-    const paymentPrice = screen.getByText('1000원');
-    expect(paymentPrice).toBeInTheDocument();
+    const paymentPrice = screen.getByTestId('payment-price');
+    expect(paymentPrice).toHaveTextContent('1000원');
   });
   test('수평바가 존재 한다.', () => {
     render(<Default />);
@@ -19,11 +19,10 @@ describe('Payment 컴포넌트의 Default 테스트', () => {
 });
 
 describe('Payment 컴포넌트의 OrderPage 스토리북 검증 테스트', () => {
-  test('1000원 Text가 존재한다', async () => {
+  test('결제예상금액은 1000원이다', async () => {
     render(<OrderPage />);
-    const paymentPrice = screen.getByText('1000원');
-
-    expect(paymentPrice).toBeInTheDocument();
+    const paymentPrice = screen.getByTestId('payment-price');
+    expect(paymentPrice).toHaveTextContent('1000원');
   });
   test('수평바가 존재 한다.', () => {
     render(<OrderPage />);
@@ -32,16 +31,16 @@ describe('Payment 컴포넌트의 OrderPage 스토리북 검증 테스트', () =
   });
   test('결제하기 버튼이 존재한다.', () => {
     render(<OrderPage />);
-    const paymentButton = screen.getByRole('button');
-    expect(paymentButton).toHaveTextContent('결제하기');
+    const paymentButton = screen.getByRole('button', { name: '결제하기' });
+    expect(paymentButton).toBeInTheDocument();
   });
 });
 
 describe('Payment 컴포넌트의 CartPage 스토리북 테스트', () => {
-  test('1000원 Text가 존재한다', async () => {
+  test('결제예상금액은 1000원이다', async () => {
     render(<CartPage />);
-    const paymentPrice = screen.getByText('1000원');
-    expect(paymentPrice).toBeInTheDocument();
+    const paymentPrice = screen.getByTestId('payment-price');
+    expect(paymentPrice).toHaveTextContent('1000원');
   });
 
   test('수평바가 존재 한다.', async () => {
@@ -49,9 +48,9 @@ describe('Payment 컴포넌트의 CartPage 스토리북 테스트', () => {
     const horizontalLine = screen.getByRole('banner');
     expect(horizontalLine).toBeInTheDocument();
   });
-  test('주문하기 버튼 텍스트는 주문하기 1개이다.', async () => {
+  test('주문하기 1개 버튼이 존재한다.', async () => {
     render(<CartPage />);
-    const orderButton = screen.getByRole('button');
-    expect(orderButton).toHaveTextContent('주문하기 1개');
+    const orderButton = screen.getByRole('button', { name: '주문하기 1개' });
+    expect(orderButton).toBeInTheDocument();
   });
 });
