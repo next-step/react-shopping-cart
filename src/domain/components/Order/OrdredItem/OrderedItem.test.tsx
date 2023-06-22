c7fd3cd4c5acb5c541e049b6d52de83309d439a8
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-22 10:05:51 +0900

    refactor : OrderedItem 테스트 수정

--- src/domain/components/Order/OrdredItem/OrderedItem.test.tsx
+++ src/domain/components/Order/OrdredItem/OrderedItem.test.tsx
@@ -6,26 +6,28 @@ import { render } from 'test/rtkProvider';
 const { Default } = composeStories(stories);
 
 describe('OrderedItem 스토리북 렌더링 검증 테스트', () => {
-  test('냉면용기 (대) Text가 존재한다', () => {
+  test('상품이름은 냉면용기 (대) 이다.', () => {
     render(<Default />);
-    const productName = screen.getByText('냉면용기(대)');
-    expect(productName).toBeInTheDocument();
+    const productName = screen.getByTestId('order-name');
+    expect(productName).toHaveTextContent('냉면용기(대)');
   });
 
-  test('수량:1 Text가 존재한다', () => {
+  test('상품 수량은 수량:1 이다.', () => {
     render(<Default />);
-    const amount = screen.getByText('수량:1');
-    expect(amount).toBeInTheDocument();
+    const amount = screen.getByTestId('order-amount');
+    expect(amount).toHaveTextContent('수량:1');
   });
+  // 컴포넌트 잘못설계된게 인지됨
 
-  test('가격: 83700원 Text가 존재한다.', () => {
+  test('상품 가격은 : 83700원 이다.', () => {
     render(<Default />);
-    const price = screen.getByText('가격: 83700원');
-    expect(price).toBeInTheDocument();
+    const price = screen.getByTestId('order-price');
+    expect(price).toHaveTextContent('가격: 83700원');
   });
-  test('상품 이미지가 존재한다.', () => {
+  // 컴포넌트 잘못설계된게 인지됨
+  test('냉면용기(대) 상품 이미지가 존재한다.', () => {
     render(<Default />);
     const image = screen.getByRole('img');
-    expect(image).toBeInTheDocument();
+    expect(image).toHaveAccessibleName('냉면용기(대)');
   });
 });
