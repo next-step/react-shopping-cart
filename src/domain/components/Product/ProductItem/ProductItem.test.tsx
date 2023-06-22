5754b81918a99288fb044b4b30e37bc67df0a531
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-22 10:43:40 +0900

    refactor :ProductItem test 수정

--- src/domain/components/Product/ProductItem/ProductItem.test.tsx
+++ src/domain/components/Product/ProductItem/ProductItem.test.tsx
@@ -6,24 +6,24 @@ import { render } from 'test/rtkProvider';
 const { Item } = composeStories(stories);
 
 describe('ProductItem 스토리북 렌더링 테스트', () => {
-  test('상품의 이미지가 존재 한다', () => {
+  test('냉면용기 (대) 상품 이미지가 존재한다', () => {
     render(<Item />);
     const image = screen.getByRole('img');
-    expect(image).toBeInTheDocument();
+    expect(image).toHaveAccessibleName('냉면용기(대)');
   });
-  test('냉면 용기(대) Text가 존재한다.', () => {
+  test('상품이름은 냉면 용기(대)이다', () => {
     render(<Item />);
-    const name = screen.getByText('냉면용기(대)');
-    expect(name).toBeInTheDocument();
+    const name = screen.getByTestId('product-name');
+    expect(name).toHaveTextContent('냉면용기(대)');
   });
-  test('83000 텍스트가 존재한다.', () => {
+  test('상품가격은 83700원이다.', () => {
     render(<Item />);
-    const price = screen.getByText('냉면용기(대)');
-    expect(price).toBeInTheDocument();
+    const price = screen.getByTestId('product-price');
+    expect(price).toHaveTextContent('83700원');
   });
   test('장바구니 아이콘 버튼 존재한다.', () => {
     render(<Item />);
-    const cartButton = screen.getByRole('button');
+    const cartButton = screen.getByRole('button', { name: 'cart.svg' });
     expect(cartButton).toBeInTheDocument();
   });
 });
