63ad2659e6014b85bf480d02dc931dcf722f5329
Seung-wan <tmddhks0104@gmail.com>
2023-07-07 02:10:02 +0900

    fix: add constant in axiosInstnace

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173';
+const LOCAL_API_BASE_URL = 'http://localhost:5173';
+const TIMEOUT = 3_000;
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || LOCAL_API_BASE_URL;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 3_000,
+  timeout: TIMEOUT,
 });
 
 export default axiosInstance;
