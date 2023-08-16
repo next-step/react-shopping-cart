65765b6120b8bb8f99f6372a386c72f603483672
Seung-wan <tmddhks0104@gmail.com>
2023-08-17 01:36:09 +0900

    fix: Add React.memo at Toast component

--- frontend/src/components/common/Toast/Toast.tsx
+++ frontend/src/components/common/Toast/Toast.tsx
@@ -1,7 +1,13 @@
-import { Props } from '@/components/common/Toast/Toast.types';
+import { memo } from 'react';
 
 import * as Styled from '@/components/common/Toast/Toast.styled';
 
-export default function Toast({ message }: Props) {
+type Props = {
+  message: string;
+};
+
+function Toast({ message }: Props) {
   return <Styled.Toast>{message}</Styled.Toast>;
 }
+
+export default memo(Toast);
