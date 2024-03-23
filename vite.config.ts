79ca9b69f65f99e00d633d59dca0378e6f2741b6
sasha1107 <sasha1107@naver.com>
2024-03-23 16:27:11 +0900

    chore: set svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   base: "/react-shopping-cart/",
   resolve: {
     alias: {
