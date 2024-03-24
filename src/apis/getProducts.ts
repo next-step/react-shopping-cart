6f2b5afb84303c188e82d75e6964a9b306adfb3f
Jay Lee <roof2586@naver.com>
2024-03-24 18:23:24 +0900

    chore: add comment

--- src/apis/getProducts.ts
+++ src/apis/getProducts.ts
@@ -1,10 +1,9 @@
-// delay
-async function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import delay from '@/utils/delay';
+
+/* DEV: skeleton ui 테스트를 위해 delay 함수를 사용하여 1초 뒤에 데이터를 반환하도록 설정 */
 
 export default function getProducts() {
-  return fetch('/products')
+  return fetch(`${import.meta.env.VITE_NEXTSTEP_API_URL}/products`)
     .then((res) => res.json())
     .then(async (data) => {
       await delay(1000);
