e79358df113d0c67697a3bdc9f41ef2c914fd283
Jay Lee <roof2586@naver.com>
2024-03-24 17:10:49 +0900

    chore: rename files

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,4 +1,4 @@
-import { Product } from './product.entity';
+import { Product } from './product.model';
 
 interface OrderDetail extends Product {
   quantity: number;
--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -3,4 +3,5 @@ export interface Product {
   name: string;
   price: number;
   imageUrl: string;
+  isInCart: boolean;
 }
