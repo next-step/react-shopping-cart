4620e8e255b18c6066faf7ede18a556961ce38c7
jiyeon <ejy1105@gmail.com>
2024-03-24 12:50:33 +0900

    chore: worker.start

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start({ onUnhandledRequest: 'bypass' }) //처리되지 않은 요청에 대한 worn, error, bypass
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
