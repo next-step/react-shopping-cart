08ac4e5aec9ef8f19879fe35feae3a0159f8e3ec
jiyeon <ejy1105@gmail.com>
2024-03-24 21:10:17 +0900

    feat: handler get 요청 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,13 @@
 import { http, HttpResponse } from 'msw'
 import list from './db.json'
 
-export const handlers = [http.get('/products', () => HttpResponse.json(list))]
+export const handlers = [
+  http.get('/products', () => HttpResponse.json(list.products)),
+  http.get('/products/:id', ({ params }) => {
+    const { id } = params
+    const filtered = list.products.filter((item, _) => item.id === Number(id))
+
+    return HttpResponse.json(filtered)
+  }),
+  http.get('/carts', () => HttpResponse.json(list.carts)),
+]
