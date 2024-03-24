e3acc6c0fa7317bc5b83cb025beef8291e568ae9
Jay Lee <roof2586@naver.com>
2024-03-24 18:26:09 +0900

    chore: remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -15,8 +15,6 @@ async function enableMocking() {
 
   const { worker } = await import('./mocks/browser');
 
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
   return worker.start();
 }
 
