724a5605c386583920d2b713eb5e7495d8c5de88
jiyeon <ejy1105@gmail.com>
2024-03-24 21:29:58 +0900

    feat: context order 타입 수정

--- src/context/cartsContext.tsx
+++ src/context/cartsContext.tsx
@@ -6,9 +6,13 @@ export interface Product {
   price: number
   imageUrl: string
 }
+export interface Order {
+  id: number
+  orderDetails: Array<Product & { quantity: number }>
+}
 export interface MyOrder {
   carts: Array<Product>
-  order: Array<Product>
+  order: Array<Order>
 }
 export const CartsContext = createContext<MyOrder>({ carts: [], order: [] })
 export const UpdateCartsContext = createContext<(payload: MyOrder) => void>(() => {})
