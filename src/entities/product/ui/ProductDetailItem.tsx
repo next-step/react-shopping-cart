20e19a541673108d7fceb33c40f71a5e0a259e8c
Gn0lee <jinho605@gmail.com>
2024-03-26 02:35:35 +0900

    test : 상품 상세페이지 테스트 추가

--- src/entities/product/ui/ProductDetailItem.tsx
+++ src/entities/product/ui/ProductDetailItem.tsx
@@ -1,11 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
 import { Product } from 'src/entities/product/type/product.type';
 import { formatPriceToKRW } from 'src/shared/lib/format';
 import usePostCartItemMutation from 'src/entities/cart/hooks/usePostCartItemMutation';
+import useAlertStore from 'src/shared/store/useAlertStore';
 
 export interface ProductDetailItemProps extends Product {}
 
 export default function ProductDetailItem({ name, price, imageUrl, id }: ProductDetailItemProps) {
-	const { mutate: postCartItem } = usePostCartItemMutation();
+	const navigate = useNavigate();
+
+	const openAlert = useAlertStore.use.open();
+
+	const { mutate: postCartItem } = usePostCartItemMutation({
+		onSuccess: () => {
+			openAlert({
+				message: '장바구니에 상품이 추가되었습니다.\n장바구니로 이동하시겠습니까?',
+				title: '장바구니',
+				confirm: () => {
+					navigate('/cart');
+				},
+			});
+		},
+	});
 
 	const handleClickCart = () => {
 		postCartItem({ product: { name, price, imageUrl, id } });
