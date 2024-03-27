7bae9dd62807788380dbab1d95021fa043e56d79
jiyeon <ejy1105@gmail.com>
2024-03-27 22:52:02 +0900

    fix: order 피드백 수정

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -1,29 +1,37 @@
 import { useContext, useEffect, useState } from 'react'
 import { CartsContext, UpdateCartsContext } from '../../context/cartsContext'
 import Modal from '../common/Modal'
+import { useQuery } from '@tanstack/react-query'
+import { fetchOrderList } from '../../api/cart'
 
 const OrderList = () => {
   const cartsContext = useContext(CartsContext)
   const updateCartsContext = useContext(UpdateCartsContext)
 
-  const [isModal, setIsModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
 
-  const FetchOrderList = async () => {
-    const response = await fetch('/orders')
-    const jsonData = await response.json()
-    updateCartsContext({ ...cartsContext, order: jsonData })
-  }
+  const { data: orderList } = useQuery({
+    queryKey: ['orderList'],
+    queryFn: () => {
+      return fetchOrderList()
+    },
+    retry: 3,
+  })
+
   useEffect(() => {
-    FetchOrderList()
-  }, [])
+    if (orderList) {
+      updateCartsContext({ ...cartsContext, order: orderList })
+    }
+  }, [orderList])
+
   return (
     <section className="order-section">
-      {isModal && (
+      {openModal && (
         <Modal
           props={{
-            isOpen: isModal,
+            isOpen: openModal,
             title: '장바구니에 담았어요!',
-            setModalStatus: () => setIsModal((state) => !state),
+            setModalStatus: () => setOpenModal((state) => !state),
           }}
         />
       )}
@@ -39,24 +47,28 @@ const OrderList = () => {
         </div>
         {cartsContext.order.map((item, idx) => (
           <div className="order-list-item" key={item.id}>
-            <div className="flex gap-15 mt-10">
-              <img className="w-144 h-144" src={item.orderDetails[idx].imageUrl} alt={item.orderDetails[idx].name} />
-              <div className="flex-col gap-15">
-                <span className="order-name">{item.orderDetails[idx].name}</span>
-                <span className="order-info">
-                  {item.orderDetails[idx].price}원 / 수량: {item.orderDetails[idx].quantity}개
-                </span>
+            {item.orderDetails.map((order) => (
+              <div key={order.name} className="order-row">
+                <div className="flex gap-15 mt-10 custom">
+                  <img className="w-144 h-144" src={order.imageUrl} alt={order.name} />
+                  <div className="flex-col gap-15">
+                    <span className="order-name">{order.name}</span>
+                    <span className="order-info">
+                      {order.price}원 / 수량: {order.quantity}개
+                    </span>
+                  </div>
+                </div>
+                <button
+                  className="primary-button-small flex-center self-start"
+                  onClick={() => {
+                    setOpenModal(true)
+                    updateCartsContext({ ...cartsContext, carts: [...cartsContext.carts, order] })
+                  }}
+                >
+                  장바구니
+                </button>
               </div>
-            </div>
-            <button
-              className="primary-button-small flex-center self-start"
-              onClick={() => {
-                setIsModal(true)
-                updateCartsContext({ ...cartsContext, carts: [...cartsContext.carts, item.orderDetails[idx]] })
-              }}
-            >
-              장바구니
-            </button>
+            ))}
           </div>
         ))}
       </div>
